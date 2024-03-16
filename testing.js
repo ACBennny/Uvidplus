ce4354c87cd78712698346fd6ce2d742e8f2d87c
ACBennny <acbennny1@gmail.com>
2024-03-16 14:18:23 -0300

    Currently working on a search bar for Uvid
    Although search engine's are generally server based, I still attempt on on client side.
    16th March, 2024

---
+++ testing.js
@@ -0,0 +1,30 @@
+
+
+
+//  trying to test the search form
+
+// function whatToSend()
+// {
+//     const url = 'http://127.0.0.1:5500/test_receive.html?';
+//     const formSend = document.querySelector('.testbox');
+//     const formSendBtn = formSend.querySelector('.sendBtn');
+//     formSend.addEventListener("submit" , whatToSend);
+//     formSendBtn.addEventListener("click" , whatToSend);
+//         const formSendInput = formSend.querySelector('#searchBoxId');
+//         const inputVal = formSendInput.value;
+//         console.log("sending..");
+//         const query = new URLSearchParams();
+//         query.append('key' , inputVal);
+//         const queryString = query.toString();
+//         const toUrl = url + queryString;
+
+//         setTimeout(() => 
+//         {
+//             window.location.href = toUrl;
+//         } ,1500);
+// }
+
+
+
+
+
