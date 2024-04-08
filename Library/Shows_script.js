31fdb067416a8c7fb79f1f91eb3e9e0a7a6d3e7f
ACBennny <acbennny1@gmail.com>
2024-04-08 03:36:28 -0300

    Updated Trailer Modal
    Repositioned the "close" button for the trailer modal so it's easier for users to see and interact with.

--- Library/Shows_script.js
+++ Library/Shows_script.js
@@ -56,10 +56,12 @@
         trailerBdr.classList.add("trailer_Bdr");
         trailerBdr.innerHTML =
         `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="closeTrailer">
-                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
-            </svg>
-            <iframe class="trailer_Box" src="${trailerSrc}" title="${trailerTitle}" frameborder="0"  allowfullscreen></iframe>
+            <div class="trailer_Box">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="closeTrailer">
+                    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                </svg>
+                <iframe class="trailer_Frame" src="${trailerSrc}" title="${trailerTitle}" frameborder="0"  allowfullscreen></iframe>
+            </div>
         `;
 
         troBase.appendChild(trailerBdr);
