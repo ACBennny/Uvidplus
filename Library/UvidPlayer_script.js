b70c8a7550a32076ce5dc740b8c1134f33bdd0be
ACBennny <acbennny1@gmail.com>
2024-04-17 16:22:26 -0300

    Landing page
    Currently working a fix for the "background-attachment" property

--- Library/UvidPlayer_script.js
+++ Library/UvidPlayer_script.js
@@ -131,64 +131,61 @@
                 </div>
 
                 <div class="settings">
-                  <div data-label="settingHome">
-                    <ul>
-                      <li data-label="speed">
-                        <span> Speed </span>
-                        <span class="">
-                          arrow_forward_ios
+                    <div data-label="settingHome">
+                        <ul>
+                            <li data-label="speed">
+                                <span> Speed </span>
+                                <span class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="">
+                                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                                    </svg>
+                                </span>
+                            </li>
+                            <li data-label="quality">
+                                <span> Quality </span>
+                                <span class="material-symbols-outlined icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="">
+                                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                                    </svg>
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="playback" data-label="speed" hidden>
+                        <span>
+                            <i class="fa-solid fa-caret-right" data-label="settingHome">
+                            </i>
+                            <span>Playback Speed </span>
                         </span>
-                      </li>
-                      <li data-label="quality">
-                        <span> Quality </span>
-                       <span class="material-symbols-outlined icon">
-                        arrow_forward_ios
-                      </span>
-                      </li>
-                    </ul>
-                  </div>
-                  <div class="playback" data-label="speed" hidden>
-                    <span>
-                      <i class="fa-solid fa-caret-right" data-label="settingHome">
-                      </i>
-                      <span>Playback Speed </span>
-                    </span>
-                    <ul>
-                      <li data-speed="0.25">0.25</li>
-              
-                      <li data-speed="0.5">0.5</li>
-              
-                      <li data-speed="0.75">0.75</li>
-              
-                      <li data-speed="1" class="active">Normal</li>
-              
-                      <li data-speed="1.25">1.25</li>
-              
-                      <li data-speed="1.5">1.5</li>
-              
-                      <li data-speed="1.75">1.75</li>
-              
-                      <li data-speed="2">2</li>
-                    </ul>
-                  </div>
-                  <div data-label="quality">
-                    <span>
-                      <i class="fa-solid fa-caret-right" data-label="settingHome">
-                      </i>
-                      <span>Playback Quality </span>
-                    </span>
-                    <ul>
-                      <li data-quality="auto" class="active">auto</li>
-                    </ul>
-                  </div>
+                        <ul>
+                            <li data-speed="0.25">0.25</li>
+                            <li data-speed="0.5">0.5</li>
+                            <li data-speed="0.75">0.75</li>
+                            <li data-speed="1" class="active">Normal</li>
+                            <li data-speed="1.25">1.25</li>
+                            <li data-speed="1.5">1.5</li>
+                            <li data-speed="1.75">1.75</li>
+                            <li data-speed="2">2</li>
+                        </ul>
+                    </div>
+                    <div data-label="quality">
+                        <span>
+                            <i class="fa-solid fa-caret-right" data-label="settingHome">
+                            </i>
+                            <span>Playback Quality </span>
+                        </span>
+                        <ul>
+                            <li data-quality="auto" class="active">auto</li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="captions">
-                  <div class="caption">
-                    <span>Select Subtitle</span>
-                    <ul>
-                      
-                    </ul>
-                  </div>
+                    <div class="caption">
+                        <span>Select Subtitle</span>
+                        <ul>
+                        
+                        </ul>
+                    </div>
                 </div>
             `;
 
