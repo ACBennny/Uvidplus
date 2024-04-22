8884b5e8357b98649b8eec6e57397c3509d706b7
ACBennny <acbennny1@gmail.com>
2024-04-22 02:00:17 -0300

    Video player
    Replaced sample video file as the music used wasn't professional

--- Library/UvidPlayer_script.js
+++ Library/UvidPlayer_script.js
@@ -25,12 +25,12 @@
                     <div class="playPause_fullscreen_VidPc"></div>
                     <div class="mobile_controls_box">
                         <div class="options center mobile_controls">
-                            <button class="skip-backward">
+                            <button class="skip-backward" title="skip backward" aria-label="skip backward">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="skipBackwardIcon">
                                     <path d="M459.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29V96c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4L288 214.3V256v41.7L459.5 440.6zM256 352V256 128 96c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160C4.2 237.5 0 246.5 0 256s4.2 18.5 11.5 24.6l192 160c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29V352z"/>
                                 </svg>
                             </button>
-                            <button class="play-pause">
+                            <button class="play-pause" title="play" aria-label="play">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 512" class="playIcon">
                                     <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/>
                                 </svg>
@@ -38,7 +38,7 @@
                                     <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
                                 </svg>
                             </button>
-                            <button class="skip-forward">
+                            <button class="skip-forward" title="skip forward" aria-label="skip forward">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="skipForwardIcon">
                                     <path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416V96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3V256v41.7L52.5 440.6zM256 352V256 128 96c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29V352z"/>
                                 </svg>
@@ -204,6 +204,7 @@
             const progress_Bar = video_player.querySelector(".progress-bar");
             const skipBackward = video_player.querySelectorAll(".skip-backward .skipBackwardIcon");
             const skipForward = video_player.querySelectorAll(".skip-forward .skipForwardIcon");
+            const playPauseBtnBox = video_player.querySelectorAll(".play-pause");
             const playPauseBtn = video_player.querySelectorAll(".play-pause svg");
             const playBtn = video_player.querySelectorAll(".play-pause .playIcon");
             const pauseBtn = video_player.querySelectorAll(".play-pause .pauseIcon");
@@ -215,6 +216,7 @@
             const current = video_player.querySelector(".current-time");
             const totalDuration = video_player.querySelector(".video-duration");
             const picture_in_picutre = video_player.querySelector(".pic-in-pic .picInPicIcon");
+            const fullScreenBtnBox = video_player.querySelectorAll(".fullscreenBtn");
             const fullScreenBtn = video_player.querySelectorAll(".fullscreenBtn svg");
             const fullScreenExpandBtn = video_player.querySelector(".fullscreenBtn .expandIcon");
             const fullScreenContractBtn = video_player.querySelector(".fullscreenBtn .compressIcon");
@@ -390,10 +392,20 @@
                 {
                     fullScreenContractBtn.classList.add("hide");
                     fullScreenExpandBtn.classList.remove("hide");
+                    fullScreenBtnBox.forEach(box => 
+                    {
+                        box.title = "enter fullscreen";
+                        box.ariaLabel = "enter fullscreen";
+                    });
                     return document.exitFullscreen();
                 }
                 fullScreenExpandBtn.classList.add("hide");
                 fullScreenContractBtn.classList.remove("hide");
+                fullScreenBtnBox.forEach(box => 
+                {
+                    box.title = "exit fullscreen";
+                    box.ariaLabel = "exit fullscreen";
+                });
                 video_player.requestFullscreen();
             }
 
@@ -503,70 +515,6 @@
                 hideControls();
             });
 
-            // Rewinding video
-            skipBackward.forEach(btn => 
-            {
-                btn.addEventListener("click", () => 
-                {
-                    videoSkip(-5);
-                });
-            });
-        
-            // Fast forwarding video
-            skipForward.forEach(btn => 
-            {
-                btn.addEventListener("click", () => 
-                {
-                    videoSkip(5);
-                });
-            });
-        
-            playPauseBtn.forEach(btn => 
-            {
-                btn.addEventListener("click", () => 
-                {
-                    toggleVidPlay();
-                });
-            });
-
-            // Pause/play (click) For Pc/ wide screen users only 
-            pcControlTapBox.addEventListener("click" , () => 
-            {
-                toggleVidPlay();
-            });
-        
-            // Playing Video
-            mainVideo.addEventListener("play", () => 
-            {
-                playBtn.forEach(btn => 
-                {
-                    btn.classList.add("hide");
-                });
-                pauseBtn.forEach(btn => 
-                {
-                    btn.classList.remove("hide");
-                });
-                video_player.classList.add("show-controls");
-                clearTimeout(ctrltimer);
-                hideControls();
-            });
-        
-            // Paused Video
-            mainVideo.addEventListener("pause", () => 
-            {
-                playBtn.forEach(btn => 
-                {
-                    btn.classList.remove("hide");
-                });
-                pauseBtn.forEach(btn => 
-                {
-                    btn.classList.add("hide");
-                });
-                video_player.classList.add("show-controls");
-                clearTimeout(ctrltimer);
-                hideControls();
-            });
-
             const formatTime = time => 
             {
                 let seconds = Math.floor(time % 60),
@@ -652,6 +600,81 @@
             mainVideo.addEventListener('canplay', () => 
             {
                 loader.style.display = "none";
+            });
+
+            // Rewinding video
+            skipBackward.forEach(btn => 
+            {
+                btn.addEventListener("click", () => 
+                {
+                    videoSkip(-5);
+                });
+            });
+        
+            // Fast forwarding video
+            skipForward.forEach(btn => 
+            {
+                btn.addEventListener("click", () => 
+                {
+                    videoSkip(5);
+                });
+            });
+        
+            playPauseBtn.forEach(btn => 
+            {
+                btn.addEventListener("click", () => 
+                {
+                    toggleVidPlay();
+                });
+            });
+
+            // Pause/play (click) For Pc/ wide screen users only 
+            pcControlTapBox.addEventListener("click" , () => 
+            {
+                toggleVidPlay();
+            });
+        
+            // Playing Video
+            mainVideo.addEventListener("play", () => 
+            {
+                window.scrollTo(0, 0);
+                playBtn.forEach(btn => 
+                {
+                    btn.classList.add("hide");
+                });
+                pauseBtn.forEach(btn => 
+                {
+                    btn.classList.remove("hide");
+                });
+                playPauseBtnBox.forEach(box => 
+                {
+                    box.title = "pause";
+                    box.ariaLabel = "pause";
+                });
+                video_player.classList.add("show-controls");
+                clearTimeout(ctrltimer);
+                hideControls();
+            });
+        
+            // Paused Video
+            mainVideo.addEventListener("pause", () => 
+            {
+                playBtn.forEach(btn => 
+                {
+                    btn.classList.remove("hide");
+                });
+                pauseBtn.forEach(btn => 
+                {
+                    btn.classList.add("hide");
+                });
+                playPauseBtnBox.forEach(box => 
+                {
+                    box.title = "play";
+                    box.ariaLabel = "play";
+                });
+                video_player.classList.add("show-controls");
+                clearTimeout(ctrltimer);
+                hideControls();
             });
 
             volumeSlider.addEventListener("input", e => 
