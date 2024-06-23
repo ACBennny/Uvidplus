abab2787286bc897b6114ec0d056d292afa240ed
ACBennny <acbennny1@gmail.com>
2024-06-23 05:29:58 -0300

    Update Main.js

--- Main.js
+++ Main.js
@@ -1280,7 +1280,7 @@
                                     </svg>
                                 </div>
                                 <div class="genAtnModalOptTextBox ">
-                                    <span class="genAtnModalOptText ">Sample A</span>
+                                    <span class="genAtnModalOptText ">Favourites</span>
                                 </div>
                             </button>
                             <button data-list="" class="genAtnModalOptBox plItem" title="" aria-label="">
@@ -1290,7 +1290,17 @@
                                     </svg>
                                 </div>
                                 <div class="genAtnModalOptTextBox ">
-                                    <span class="genAtnModalOptText ">Sample B</span>
+                                    <span class="genAtnModalOptText ">Old Shows</span>
+                                </div>
+                            </button>
+                            <button data-list="" class="genAtnModalOptBox plItem" title="" aria-label="">
+                                <div class="genAtnModalOptIconBox">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="genAtnModalOptIcon">
+                                        <path d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"/>
+                                    </svg>
+                                </div>
+                                <div class="genAtnModalOptTextBox ">
+                                    <span class="genAtnModalOptText ">Quests</span>
                                 </div>
                             </button>
                         </div>
