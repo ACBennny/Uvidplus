49b47123ddbc254442ed46ba2d963bd03a3bc506
ACBennny <acbennny1@gmail.com>
2024-07-05 16:51:16 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -392,10 +392,10 @@
                                                 <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
                                             </svg>
                                         </div>
-                                        <div class="navBarCompanylogo logo logo1">
+                                        <a href="/Home.html" class="Companylogo navBarCompanylogo">
                                             <h1 class="lhead">U</h1>
                                             <h1 class="lname">vid</h1>
-                                        </div>
+                                        </a>
                                     </div>
                                 </section>
 
