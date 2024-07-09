d65c4b14757e11a4bdc82343ff066e86a255c336
ACBennny <acbennny1@gmail.com>
2024-07-10 00:10:47 +0100

    Trending page
    Progress continues on the page

---
+++ Trending/trending.js
@@ -0,0 +1,9 @@
+
+/***************************************************************
+ * This will hold the code for the Trending page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
