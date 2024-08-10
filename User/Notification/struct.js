7d96de557193b0bcfc502ff7c807f063397ff580
ACBennny <acbennny1@gmail.com>
2024-08-10 14:58:01 +0100

    User Pages, Files etcetera
    1. I created some files to serve the user pages.
    2. The structure (struct) files will be included in the folder for each user page.

---
+++ User/Notification/struct.js
@@ -0,0 +1,11 @@
+/***************************************************************
+ * This will hold the code for the Struct of the Notification page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+    const userPagesStruct =
+    ``;
