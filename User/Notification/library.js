8a1c5bbc274b3882d1ad6ecf76df3d8c4d38a9d8
ACBennny <acbennny1@gmail.com>
2024-08-16 23:44:14 +0100

    Notification Library
    I created a file to hold the notification properties.

---
+++ User/Notification/library.js
@@ -0,0 +1,39 @@
+/***************************************************************
+ * This will hold the code for General functions of the Notification page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+    const notificationInventory = 
+    [
+        {
+            added_date: `1 min ago`,
+            read_status: `unread`,
+            thumbnail: `/Images/Uvid_bcg1.jpg`,
+            main_topic: `Welcome to Uvid`,
+            sub_topic: `We are glad you joined us. Browse and watch your favourite movies and tv shows.`,
+            action_text: `Explore`,
+            action_link: `/Home.html`,
+        },
+        {
+            added_date: `just now`,
+            read_status: `unread`,
+            thumbnail: `/Images/Uvid_bcg2.jpg`,
+            main_topic: `Switch Profiles`,
+            sub_topic: `You can now create and customize up to five different profiles.`,
+            action_text: `Try it Out`,
+            action_link: ``,
+        },
+        {
+            added_date: `just now`,
+            read_status: `unread`,
+            thumbnail: `/Library/Anime/Watch/OnePiece/img/1.jpg`,
+            main_topic: `One Piece`,
+            sub_topic: `Episode 1115 is now available.`,
+            action_text: `Watch Now`,
+            action_link: ``,
+        }
+    ]
