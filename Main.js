56b84671115a27aba3a856e38e789e443bcbad27
ACBennny <acbennny1@gmail.com>
2024-08-22 23:32:19 +0100

    Navbar Notification
    I noticed another bug, where multiple default notifications got added to the DOM whereas only one was needed.
    To rectify this I decided to enclose the previous div in a button and disable the button after it has been clicked once.

--- Main.js
+++ Main.js
@@ -384,12 +384,14 @@
                 <div class="navBarNotificationMinor">
                     <div class="navBarNotificationHeaderBdr">
                         <div class="navBarNotificationHeaderBox">
-                            <div class="navBarNotificationHeaderIconBox">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="navBarNotificationHeaderIconSvg">
-                                    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-                                </svg>
-                            </div>
-                            <p class="navBarNotificationHeaderText markAllNotificationsAsRead">Mark all as read</p>
+                            <button class="navBarNotificationHeaderBtn markAllNotificationsAsRead">
+                                <div class="navBarNotificationHeaderIconBox">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="navBarNotificationHeaderIconSvg">
+                                        <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
+                                    </svg>
+                                </div>
+                                <p class="navBarNotificationHeaderText ">Mark all as read</p>
+                            </button>
                         </div>
                     </div>
                     <div class="navBarNotificationCtntBdr">
@@ -874,6 +876,7 @@
                     let markAllNotificationsAsRead = document.querySelector(".markAllNotificationsAsRead");
                     markAllNotificationsAsRead.addEventListener("click" , () => 
                     {
+                        markAllNotificationsAsRead.disabled = true;
                         navBarNotificationCardBdr.forEach((bdr) => 
                         {
                             bdr.remove();
