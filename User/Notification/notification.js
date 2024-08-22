f3f0d81a267164ee6c7f0221a75c317af9261a78
ACBennny <acbennny1@gmail.com>
2024-08-22 19:20:24 +0100

    Notification Page | User Pages
    I updated the functions to correspond with the navbar notification functions.

--- User/Notification/notification.js
+++ User/Notification/notification.js
@@ -17,60 +17,79 @@
         {
             fetchUserNotification();
         });
+        notificationLibraryScriptTag.onerror = function() 
+        {
+            errorLoadingUserNotifications();
+        };
+
+        // For error events
+        function errorLoadingUserNotifications()
+        {
+            notification(`notifyBad` , `An error occurred while loading notifications`);
+            openNavBarNotificationBtn.addEventListener("click" , failedLoadErrorMsg);
+        }
 
         document.body.appendChild(notificationLibraryScriptTag);
-    }
 
-    function fetchUserNotification()
-    {
-        for(let i = 0; i < notificationInventory.length; i++)
+        function fetchUserNotification()
         {
-            const item = notificationInventory[i];
-            const notificationCardStruct = 
-            `
-                <div class="notification_card_bdr">
-                    <div class="notification_card_box">
-                        <div class="notification_thumbnail_bdr">
-                            <div class="notification_thumbnail_box">
-                                <img src="${item.notify_thumbnail}" alt="The thumbnail image of the '${item.notify_mainTopic}' notification" class="notification_thumbnail_img">
+            // Chekc if content of library is available
+            if(((notificationInventory === undefined) || (notificationInventory.length <= 0)))
+            {
+                errorLoadingUserNotifications();
+                return;
+            }
+
+            for(let i = 0; i < notificationInventory.length; i++)
+            {
+                const item = notificationInventory[i];
+                const notificationCardStruct = 
+                `
+                    <div class="notification_card_bdr">
+                        <div class="notification_card_box">
+                            <div class="notification_thumbnail_bdr">
+                                <div class="notification_thumbnail_box">
+                                    <img src="${item.notify_thumbnail}" alt="The thumbnail image of the '${item.notify_mainTopic}' notification" class="notification_thumbnail_img">
+                                </div>
                             </div>
-                        </div>
-                        <div class="notification_detail_bdr">
-                            <div class="notification_detail_box">
-                                <div class="notification_MainTopic_bdr">
-                                    <div class="notification_MainTopic_box">
-                                        <div class="notification_MainTopic_text">${item.notify_mainTopic}</div>
+                            <div class="notification_detail_bdr">
+                                <div class="notification_detail_box">
+                                    <div class="notification_MainTopic_bdr">
+                                        <div class="notification_MainTopic_box">
+                                            <div class="notification_MainTopic_text">${item.notify_mainTopic}</div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="notification_SubTopic_bdr">
-                                    <div class="notification_SubTopic_box">
-                                        <p class="notification_SubTopic_text">${item.notify_subTopic}</p>
+                                    <div class="notification_SubTopic_bdr">
+                                        <div class="notification_SubTopic_box">
+                                            <p class="notification_SubTopic_text">${item.notify_subTopic}</p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="notification_action_bdr">
-                                    <div class="notification_action_box">
-                                        <a href="${item.notify_actionLink}" title="${item.notify_actionText}" class="notification_action_text">${item.notify_actionText} →</a>
+                                    <div class="notification_action_bdr">
+                                        <div class="notification_action_box">
+                                            <a href="${item.notify_actionLink}" title="${item.notify_actionText}" class="notification_action_text">${item.notify_actionText} →</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            `;
-            notificationCtntBox.insertAdjacentHTML('beforeend', notificationCardStruct);
+                `;
+                notificationCtntBox.insertAdjacentHTML('beforeend', notificationCardStruct);
+            }
+            let notificationCardBdr = document.querySelectorAll(".notification_card_bdr");
+
+            // Add listener for "Mark all as read" button
+            let markAllNotificationsAsRead = document.querySelector(".markAllNotificationsAsRead");
+            markAllNotificationsAsRead.addEventListener("click" , () => 
+            {
+                notificationCardBdr.forEach((bdr) => 
+                {
+                    bdr.remove();
+                });
+                notification(`notifyGood` , `All notifications marked as read`);
+            });
+    
         }
-
-        assignNotificationListeners();
-    }
-
-
-    function assignNotificationListeners()
-    {
-        let markAllNotificationsAsRead = document.querySelector(".markAllNotificationsAsRead");
-        markAllNotificationsAsRead.addEventListener("click" , () => 
-        {
-            notification(`notifyGood` , `All notifications marked as read`);
-        });
     }
 
 
