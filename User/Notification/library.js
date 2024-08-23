68654ea5856fe0e1fb49d91e7cf295a1e3c9b034
ACBennny <acbennny1@gmail.com>
2024-08-23 16:56:26 +0100

    Update library.js

--- User/Notification/library.js
+++ User/Notification/library.js
@@ -25,14 +25,14 @@
             notify_mainTopic: `Switch Profiles`,
             notify_subTopic: `You can now create and customize up to five different profiles.`,
             notify_actionText: `Try it Out`,
-            notify_actionLink: ``,
+            notify_actionLink: `/User/Profile.html`,
         },
         {
             notify_addedDate: `just now`,
             notify_readStatus: `unread`,
             notify_thumbnail: `/Library/Anime/Watch/OnePiece/img/1.jpg`,
             notify_mainTopic: `One Piece`,
-            notify_subTopic: `Episode 1115 is now available.`,
+            notify_subTopic: `Episode 1116 is now available.`,
             notify_actionText: `Watch Now`,
             notify_actionLink: `/Library/Anime/OnePiece.html`,
         }
