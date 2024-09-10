015770656bd55de8287260de4a7113cca7cb8319
ACBennny <acbennny1@gmail.com>
2024-09-09 23:19:37 -0300

    Empty Page Status | User pages
    I noticed after a user clears notifications or watch history. the page is empty. Aside from an alert notification popping up, I felt I should add a default element to let the user know the page is empty.

--- User/History/history.js
+++ User/History/history.js
@@ -7,6 +7,26 @@
 ****************************************************************/
 
     let historyCtntBox = document.querySelector(".history_ctnt_box");
+    let historyCatalogBase = document.querySelector(".history_ctnt_box .basicCatalogBase");
+    let historyCatalogBox = document.querySelector(".history_catalog_box");
+    let emptyUserPageBdr = document.querySelector(".emptyUserPageBdr");
+    let emptyhistoryStruct = 
+    `
+        <div class="emptyUserPageBdr">
+            <div class="emptyUserPageBox">
+                <div class="emptyUserPageCtnt">
+                    <div class="emptyUserPageIconBox">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="emptyUserPageIconSvg">
+                            <path d="M88.7 223.8L0 375.8 0 96C0 60.7 28.7 32 64 32l117.5 0c17 0 33.3 6.7 45.3 18.7l26.5 26.5c12 12 28.3 18.7 45.3 18.7L416 96c35.3 0 64 28.7 64 64l0 32-336 0c-22.8 0-43.8 12.1-55.3 31.8zm27.6 16.1C122.1 230 132.6 224 144 224l400 0c11.5 0 22 6.1 27.7 16.1s5.7 22.2-.1 32.1l-112 192C453.9 474 443.4 480 432 480L32 480c-11.5 0-22-6.1-27.7-16.1s-5.7-22.2 .1-32.1l112-192z"/>
+                        </svg>
+                    </div>
+                    <div class="emptyUserPageNoteBox">
+                        <p class="emptyUserPageNoteText">Your watch history will appear here</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
 
     function startUserPage()
     {
@@ -29,21 +49,35 @@
             notification(`notifyBad` , `An error occurred while loading history`);
         }
 
+        // For inserting the empty status
+        function insertEmptyHistStatus()
+        {
+            // Clearing watch history
+            historyCatalogBase.remove();
+
+            // Inserting empty status
+            historyCtntBox.insertAdjacentHTML(`afterbegin` , emptyhistoryStruct);
+        }
+
         function fetchUserHistory()
         {
-            // Chekc if content of library is available
+            // Check if content of library is available
             if(((historyInventory == undefined) || (historyInventory.length <= 0)))
             {
                 errorLoadingUserHistory();
                 return;
             }
+
+            // Remove the empty status
+            emptyUserPageBdr.remove();
             
+            // Fetch the watch history
             for(let i = 0; i < historyInventory.length; i++)
             {
                 const item = historyInventory[i];
                 const historyCardStruct = 
                 `
-                    <div class="slide_card_base history_card">
+                    <div class="slide_card_base history_card max_sizing">
                         <div class="slide_card_bdr">
                             <div class="slide_card_box">
                                 <div class="slide_card ">
@@ -107,24 +141,28 @@
                         </div>
                     </div>
                 `;
-                historyCtntBox.insertAdjacentHTML('beforeend', historyCardStruct);
+                historyCatalogBox.insertAdjacentHTML(`beforeend`, historyCardStruct);
             }
 
             const historyCard = document.querySelectorAll(".history_card");
 
             // Function to convert time to seconds, handling both "HH:MM:SS" and "MM:SS" formats
-            function timeToSeconds(time) {
+            function timeToSeconds(time) 
+            {
                 const parts = time.split(':').map(Number);
             
                 if (parts.length === 3) 
                 {
                     // "HH:MM:SS" format
                     return parts[0] * 3600 + parts[1] * 60 + parts[2];
-                } else if (parts.length === 2) 
+                } 
+                else if (parts.length === 2) 
                 {
                     // "MM:SS" format
                     return parts[0] * 60 + parts[1];
-                } else {
+                } 
+                else 
+                {
                     notification(`notifyBad` , `Invalid time format`);
                 }
             }
@@ -157,7 +195,17 @@
                     const parentOfBtn = this.closest('.history_card');
                     if (parentOfBtn) 
                     {
-                        parentOfBtn.remove();
+                        // Remove the history card
+                        parentOfBtn.remove(); 
+
+                        // Check if there are any remaining history cards
+                        const remainingHistoryCards = document.querySelectorAll(".history_card");
+                        if (remainingHistoryCards.length === 0) 
+                        {
+                            // Inserting empty status if there are none
+                            insertEmptyHistStatus();
+                        }
+
                         notification(`notifyGood` , `Show removed from history`);
                         return;
                     }
@@ -171,10 +219,13 @@
             {
                 btn.addEventListener("click" , () => 
                 {
-                    historyCard.forEach((card) => 
-                    {
-                        card.remove();
-                    });
+                    // Disable button
+                    btn.disabled = true;
+
+                    // Insert empty status
+                    insertEmptyHistStatus();
+
+                    // Alert user
                     notification(`notifyGood` , `Your History has been cleared successfully`);
                 });
             });
