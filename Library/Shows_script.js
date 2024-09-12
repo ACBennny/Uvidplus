4969570bbc4ef8bbad259d217f262adb680bf9fc
ACBennny <acbennny1@gmail.com>
2024-09-12 11:51:30 -0300

    Update Shows_script.js

--- Library/Shows_script.js
+++ Library/Shows_script.js
@@ -162,12 +162,6 @@
                                                     </svg>
                                                     <p class="quickAtnModal_btnText">Watch Trailer</p>
                                                 </button>
-                                                <!-- <button class="quickAtnModal_btn watchShowNowBtn" title="Watch Now" aria-label="Watch Now">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="quickAtnModal_btnIcon score_btnIcon add_to_LikedShows">
-                                                        <path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zM188.3 147.1c-7.6 4.2-12.3 12.3-12.3 20.9V344c0 8.7 4.7 16.7 12.3 20.9s16.8 4.1 24.3-.5l144-88c7.1-4.4 11.5-12.1 11.5-20.5s-4.4-16.1-11.5-20.5l-144-88c-7.4-4.5-16.7-4.7-24.3-.5z"/>
-                                                    </svg>
-                                                    <p class="quickAtnModal_btnText">Watch Now</p>
-                                                </button> -->
                                                 <button class="quickAtnModal_btn add_to_Playlist openAddToWLBtn" title="Add to Watchlist" aria-label="Add to Watchlist">
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="quickAtnModal_btnIcon score_btnIcon add_to_LikedShows">
                                                         <path d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8 32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2 53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"/>
