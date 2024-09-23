d4be6f87a7eb3d6f7b4d5fbafa4f9ef5f49b7623
ACBennny <acbennny1@gmail.com>
2024-09-23 14:57:58 -0300

    Date Slider | Schedule Page
    I improved the slider's interface to make it easier for the user to view/select dates.

--- Schedule/schedule.js
+++ Schedule/schedule.js
@@ -10,8 +10,17 @@
 // DEFINITION
 
     const scheduleSelTab = document.querySelectorAll(".schedule_selTab");
+    const scheduleDateLeftBdr = document.querySelector(".schedule_dateLeftBorder");
+    const scheduleDateRightBdr = document.querySelector(".schedule_dateRightBorder");
+    const scheduleDateSlider = document.querySelector(".schedule_dateSlider");
     const scheduleDateCard = document.querySelectorAll(".schedule_dateCard");
     const scheduleGidBox = document.querySelector(".schedule_ctntGrid");
+    let boxErrorMargin = 10;
+    let isDown = false;
+    let isDragging = false;
+    let isDraggingDist = 10;
+    let startX;
+    let dateSliderScrollLeft;
 
 // CHANGING THE FILTER
 
@@ -29,21 +38,60 @@
     });
 
 
-// CUSTOM SLIDING BY DRAGGING FOE DATE SLIDER
+// SLIDER
 
-    const scheduleDateSlider = document.querySelector(".schedule_dateSlider");
-    let isDown = false;
-    let isDragging = false;
-    let isDraggingDist = 10;
-    let startX;
-    let scrollLeft;
+    // Toggling border visibility
 
+    // Initialization on page load
+     if((scheduleDateSlider.scrollWidth) > (Math.ceil((scheduleDateSlider.clientWidth)) + boxErrorMargin))
+    {
+        scheduleDateRightBdr.classList.toggle("hide");
+    }
+    if((Math.ceil(scheduleDateSlider.scrollLeft)) > boxErrorMargin)
+    {
+        scheduleDateLeftBdr.classList.toggle("hide");
+    }
+
+    function toggleScheduleBdrVisibility()
+    {
+        let scrollStart = boxErrorMargin;
+        let scrollEnd = scheduleDateSlider.scrollWidth;
+        let scrollOffWidth = Math.ceil(scheduleDateSlider.offsetWidth);
+        let currScrollLeft = Math.ceil(scheduleDateSlider.scrollLeft);
+        let currScroll = Math.ceil((scrollOffWidth + currScrollLeft));
+
+        // Hides Right button
+        if(!(((currScroll) >= scrollEnd) || ((currScroll) >= (scrollEnd - boxErrorMargin))))
+        {
+            scheduleDateRightBdr.classList.remove("hide");
+        }
+        else
+        {
+            scheduleDateRightBdr.classList.add("hide");
+        }
+
+        // Hides Left button
+        if(!(currScrollLeft <= scrollStart))
+        {
+            scheduleDateLeftBdr.classList.remove("hide");
+        }
+        else
+        {
+            scheduleDateLeftBdr.classList.add("hide");
+        }        
+    }
+    scheduleDateSlider.addEventListener("scroll" , (e) => 
+    {
+        toggleScheduleBdrVisibility();
+    });
+
+    // Slider dragging
     scheduleDateSlider.addEventListener("mousedown", (e) => 
     {
         isDown = true;
         scheduleDateSlider.classList.add("isDown");
         startX = e.pageX - scheduleDateSlider.offsetLeft;
-        scrollLeft = scheduleDateSlider.scrollLeft;
+        dateSliderScrollLeft = scheduleDateSlider.scrollLeft;
         isDragging = false;
     });
 
@@ -67,7 +115,7 @@
         e.preventDefault();
         const x = e.pageX - scheduleDateSlider.offsetLeft;
         const scrollSpeed = (x - startX) * 2;
-        scheduleDateSlider.scrollLeft = scrollLeft - scrollSpeed;
+        scheduleDateSlider.scrollLeft = dateSliderScrollLeft - scrollSpeed;
 
         // Prevent the cards from being clicked while dragging 
         if(((Math.abs(x - startX) > isDraggingDist)))
