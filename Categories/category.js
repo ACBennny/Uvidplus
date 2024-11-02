3222c73649376ff3c8e9d7a679acc4e425723e20
ACBennny <acbennny1@gmail.com>
2024-11-02 00:32:39 -0300

    Category Background Color
    I decided to use randomly generated colors for the backgrounds rather than hard coded colors.

--- Categories/category.js
+++ Categories/category.js
@@ -4,3 +4,28 @@
  * @author (Anyanwu Benedict Chukwuemeka)
  * @version (v0.01)
  *************************************************************/
+
+
+    // Definition
+    let categoriesGridCardBcg = document.querySelectorAll(".categoriesGridCardBcg");
+
+
+    // Applying a random bcg clr to the category cards
+    function genRandomColor() 
+    {
+        const letters = '0123456789abcdef';
+        let color = '#';
+        for (let i = 0; i < 6; i++) 
+        {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+
+    categoriesGridCardBcg.forEach(card => 
+    {
+        const randomColor = genRandomColor();
+        card.style.setProperty('--catBcgClr', randomColor);
+    });
+
