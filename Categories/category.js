b67aa75c83db5396f110630a6f7b6c035b87d16a
ACBennny <acbennny1@gmail.com>
2024-11-01 23:58:46 -0300

    Categories Page
    I am currently finished with the design of the cards. I am now working on it's functionality.

---
+++ Categories/category.js
@@ -0,0 +1,6 @@
+/*************************************************************
+ * This is for the general functionalities of the category page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *************************************************************/
