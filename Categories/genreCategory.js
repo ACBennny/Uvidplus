4175f2303d7464413a725d7f0d9e2962568675a1
ACBennny <acbennny1@gmail.com>
2024-11-11 08:02:49 -0400

    Update genreCategory.js

--- Categories/genreCategory.js
+++ Categories/genreCategory.js
@@ -1,5 +1,5 @@
 /*************************************************************
- * This contains the elements of the genre category
+ * This contains the elements of the genre Category
  * 
  * @author (Anyanwu Benedict Chukwuemeka)
  * @version (v0.01)
