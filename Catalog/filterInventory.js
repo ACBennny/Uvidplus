77e517c76ea833ae4461c1cac75b9688be3b2dcf
ACBennny <acbennny1@gmail.com>
2024-11-11 23:59:11 -0400

    Catalog Page
    I started working on the catalog page. A good chunk of it is ready however the main functionality, filtering is still in progress.

---
+++ Catalog/filterInventory.js
@@ -0,0 +1,181 @@
+/*************************************************************
+ * This contains the elements of the filter Catalog
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *************************************************************/
+
+
+
+let filterInv = 
+[
+    {
+        category: `Sort by`,
+        options: 
+        [
+            `Date Added`,
+            `Title`,
+            `Average Score`,
+            `Release Date`,
+        ],
+    },
+    {
+        category: `Format`,
+        options: 
+        [
+            `Tv`,
+            `Movie`,
+        ]
+    },
+    {
+        category: `Quality`,
+        options: 
+        [
+            `4K`,
+            `HD`,
+            `SD`,
+        ],
+    },
+    {
+        category: `Genres`,
+        options: 
+        [
+            `Action`,
+            `Adventure`,
+            `Anime`,
+            `Biography`,
+            `Comedy`,
+            `Crime`,
+            `Documentary`,
+            `Drama`,
+            `Family`,
+            `Fantasy`,
+            `History`,
+            `Horror`,
+            `Kids`,
+            `Music`,
+            `Mystery`,
+            `Reality`,
+            `Romance`,
+            `Sci-fi`,
+            `Thriller`,
+            `War`,
+        ],
+    },
+    {
+        category: `Year`,
+        options: 
+        [
+            `2024`,
+            `2023`,
+            `2022`,
+            `2020`,
+            `2019`,
+            `2018`,
+            `2017`,
+            `2016`,
+            `2015`,
+            `2014`,
+            `2013`,
+            `2012`,
+            `2011`,
+            `2010`,
+            `2009`,
+            `2008`,
+            `2007`,
+            `2006`,
+            `2005`,
+            `2004`,
+            `2003`,
+            `2002`,
+            `2001`,
+            `2000`,
+            `1999`,
+            `Older`,
+        ],
+    },
+    {
+        category: `Origin`,
+        options: 
+        [
+            `Argentina`,
+            `Australia`,
+            `Austria`,
+            `Belgium`,
+            `Brazil`,
+            `Canada`,
+            `China`,
+            `Czech Republic`,
+            `Denmark`,
+            `Finland`,
+            `France`,
+            `Germany`,
+            `Hong Kong`,
+            `Hungary`,
+            `India`,
+            `Ireland`,
+            `Israel`,
+            `Italy`,
+            `Japan`,
+            `Luxemborg`,
+            `Mexico`,
+            `Netherlands`,
+            `New Zealand`,
+            `Norway`,
+            `Poland`,
+            `Romania`,
+            `Russia`,
+            `South Africa`,
+            `South Korea`,
+            `Spain`,
+            `Sweden`,
+            `Switzerland`,
+            `Taiwan`,
+            `Thailand`,
+            `United Kingdom`,
+            `United States of America`,
+        ],
+    },
+    {
+        category: `Status`,
+        options: 
+        [
+            `Finished`,
+            `Releasing`,
+            `Not Yet Released`,
+            `Cancelled`,
+        ],
+    },
+    {
+        category: `Rating`,
+        options: 
+        [
+            `Rx`,
+            `R+`,
+            `R17+`,
+            `PG13`,
+            `PG`,
+            `G`,
+        ],
+    },
+    {
+        category: `Score`,
+        options: 
+        [
+            `10`,
+            `9`,
+            `8`,
+            `7`,
+            `6`,
+            `5`,
+            `4`,
+            `3`,
+            `2`,
+            `1`,
+        ],
+    }
+];
+
+
+
+
