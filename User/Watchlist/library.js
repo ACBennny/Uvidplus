c188cfff1b3cea58719e66f53f173af4db7fa19b
ACBennny <acbennny1@gmail.com>
2024-11-19 19:39:31 -0400

    Update library.js

--- User/Watchlist/library.js
+++ User/Watchlist/library.js
@@ -15,13 +15,13 @@ const watchlistInventory =
         wl_desc: `In this watchlist lies some of the greatest anime shows to ever be aired since the day anime was first introduced to the public.`,
         wl_items:
         [
-            {wl_itemId: `/Library.html?info=FullmetalAlchemistBrotherhood`},
-            {wl_itemId: `/Library.html?info=OnePiece`},
             {wl_itemId: `/Library.html?info=JujutsuKaisen`},
-            {wl_itemId: `/Library.html?info=AttackOnTitan`},
-            {wl_itemId: `/Library.html?info=DemonSlayer`},
+            {wl_itemId: `/Library.html?info=OnePiece`},
             {wl_itemId: `/Library.html?info=Naruto`},
+            {wl_itemId: `/Library.html?info=DemonSlayer`},
             {wl_itemId: `/Library.html?info=OnePunchMan`},
+            {wl_itemId: `/Library.html?info=FullmetalAlchemistBrotherhood`},
+            {wl_itemId: `/Library.html?info=AttackOnTitan`},
         ],
     },
     {
