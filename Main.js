a9910e091525d359c4e687051cb664c749ddd87a
ACBennny <acbennny1@gmail.com>
2024-11-21 19:56:36 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1596,14 +1596,14 @@
                     </div>
                     <div class="genAtnModalOptBcg addToWLItemBcg">
                         <div class="genAtnModalOptBdr addToWLItemBox">
-                            <div class="genAtnModalOptBox newWLBtn" title="Create new playlist" aria-label="Create new playlist">
+                            <div class="genAtnModalOptBox newWLBtn" title="Create Watchlist" aria-label="Create-Watchlist-btn">
                                 <div class="genAtnModalOptIconBox">
                                     <svg transform="scale(0.85)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="genAtnModalOptIcon">
                                         <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
                                     </svg>
                                 </div>
                                 <div class="genAtnModalOptTextBox ">
-                                    <span class="genAtnModalOptText ">Create new Watchlist</span>
+                                    <span class="genAtnModalOptText ">Create Watchlist</span>
                                 </div>
                             </div>
                             <div class="newWLBdr">
