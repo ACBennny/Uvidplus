11af481880f3a17c1c0103bfb216ea1461a7387d
ACBennny <acbennny1@gmail.com>
2024-11-27 00:45:01 -0400

    Update catalog.js

--- Catalog/catalog.js
+++ Catalog/catalog.js
@@ -658,13 +658,9 @@
 
         thisFilterListBox.insertAdjacentHTML(`afterbegin` , 
             `
-                <div class="wlModalAction_MenuCloseBdr catalogFilterListCloseBdr closeUserOrderOptBtn">
-                    <div class="wlModalAction_MenuCloseBox">
-                        <div class="wlModalAction_MenuCloseIcon">
-                            <svg class="wlModalAction_MenuCloseSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
-                            </svg>
-                        </div>
+                <div class="catalogFilterListCloseBdr closeUserOrderOptBtn">
+                    <div class="catalogFilterListCloseBox">
+                        <div class="catalogFilterListCloseIcon"></div>
                     </div>
                 </div>
             `
