8c79943fbaad0a418bc754913af11d079c6e026d
ACBennny <acbennny1@gmail.com>
2025-01-03 16:32:59 -0400

    Update watchlist.js

--- User/Watchlist/watchlist.js
+++ User/Watchlist/watchlist.js
@@ -43,7 +43,6 @@
     let wlModalSortTypeText;
     let wlModalSortOrderText;
     let wlModalGrid_CardBdr;
-    let wlModalGrid_CardBox;
     let closeWLModalTimer;
     let wlModalSortDfltArray = [];
     let wlModalSortUsedArray = [];
@@ -982,71 +981,70 @@
                 
                 let itemStruct = 
                 `
-                    
-                        <div class="wlModalGrid_CardBox">
-                            <div class="wlModalGrid_CardHandleBdr" draggable="true">
-                                <div class="wlModalGrid_CardHandleBox">
-                                    <div class="wlModalGrid_CardHandleIcon genDraggableHandles">
-                                        <svg class="wlModalGrid_CardHandleSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                            <path d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z"/>
-                                        </svg>
-                                    </div>
+                    <div class="wlModalGrid_CardBox">
+                        <div class="wlModalGrid_CardHandleBdr" draggable="true">
+                            <div class="wlModalGrid_CardHandleBox">
+                                <div class="wlModalGrid_CardHandleIcon genDraggableHandles">
+                                    <svg class="wlModalGrid_CardHandleSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                        <path d="M32 288c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 288zm0-128c-17.7 0-32 14.3-32 32s14.3 32 32 32l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 160z"/>
+                                    </svg>
                                 </div>
                             </div>
-                            <div onclick="window.open('${show_link}' , '_self')" class="wlModalGrid_CardCtntBdr">
-                                <div class="wlModalGrid_CardCtntBox">
-                                    <div class="wlModalGrid_CardCtntThumbBdr">
-                                        <div class="wlModalGrid_CardCtntThumbBox">
-                                            <img class="wlModalGrid_CardCtntThumbImg img_large" src="${show_background}" alt="Thumbnail image for ${show_title}">
-                                            <img class="wlModalGrid_CardCtntThumbImg img_small" src="${show_foreground}" alt="Thumbnail image for ${show_title}">
-                                        </div>
-                                    </div>
-                                    <div class="wlModalGrid_CardCtntDetBdr" data-ctnt-link="${show_link}">
-                                        <div class="wlModalGrid_CardCtntDetBox">
-                                            <div class="wlModalGrid_CardCtnt_DetTitleBox">
-                                                <div class="wlModalGrid_CardCtnt_DetTitleText">${show_title}</div>
-                                            </div>
-                                            <div class="wlModalGrid_CardCtnt_DetTagBdr">
-                                                <div class="wlModalGrid_CardCtnt_DetTagBox">
-                                                    <div class="wlModalGrid_CardCtnt_DetTagSectBox wlModalGrid_CardCtnt_DetTagScoreBox">
-                                                        <div class="wlModalGrid_CardCtnt_DetTagScoreIcon">
-                                                            <svg class="wlModalGrid_CardCtnt_DetTagScoreSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                                                                <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
-                                                            </svg>
-                                                        </div>
-                                                        <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagScoreText wlModalGridCard_Score">${show_scores}</div>
-                                                    </div>
-                                                    <div class="wlModalGrid_CardCtnt_DetTagSectBox">
-                                                        <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagTypeText wlModalGridCard_Type">${show_type}</div>
-                                                    </div>
-                                                    <div class="wlModalGrid_CardCtnt_DetTagSectBox">
-                                                        <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagReleaseText wlModalGridCard_Year">${show_year}</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="wlModalGrid_CardCtnt_DetDescBox">
-                                                <p class="wlModalGrid_CardCtnt_DetDescText">${show_description}</p>
-                                            </div>
-                                        </div>
+                        </div>
+                        <div onclick="window.open('${show_link}' , '_self')" class="wlModalGrid_CardCtntBdr">
+                            <div class="wlModalGrid_CardCtntBox">
+                                <div class="wlModalGrid_CardCtntThumbBdr">
+                                    <div class="wlModalGrid_CardCtntThumbBox">
+                                        <img class="wlModalGrid_CardCtntThumbImg img_large" src="${show_background}" alt="Thumbnail image for ${show_title}">
+                                        <img class="wlModalGrid_CardCtntThumbImg img_small" src="${show_foreground}" alt="Thumbnail image for ${show_title}">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="wlModalActionFence wlModalAction_GridCardFence">
-                                <div class="wlModalActionBase">
-                                    <div class="wlModalActionBdr">
-                                        <div class="wlModalActionBox">
-                                            <button type="button" class="wlModalActionBtn openGenMenuModalBtn" data-gen-menu-modal-type="wl_modal_cards">
-                                                <div class="wlModalActionBtnIcon">
-                                                    <svg class="wlModalActionBtnSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
-                                                        <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
-                                                    </svg>
+                                <div class="wlModalGrid_CardCtntDetBdr" data-ctnt-link="${show_link}">
+                                    <div class="wlModalGrid_CardCtntDetBox">
+                                        <div class="wlModalGrid_CardCtnt_DetTitleBox">
+                                            <div class="wlModalGrid_CardCtnt_DetTitleText">${show_title}</div>
+                                        </div>
+                                        <div class="wlModalGrid_CardCtnt_DetTagBdr">
+                                            <div class="wlModalGrid_CardCtnt_DetTagBox">
+                                                <div class="wlModalGrid_CardCtnt_DetTagSectBox wlModalGrid_CardCtnt_DetTagScoreBox">
+                                                    <div class="wlModalGrid_CardCtnt_DetTagScoreIcon">
+                                                        <svg class="wlModalGrid_CardCtnt_DetTagScoreSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                                                            <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
+                                                        </svg>
+                                                    </div>
+                                                    <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagScoreText wlModalGridCard_Score">${show_scores}</div>
                                                 </div>
-                                            </button>
+                                                <div class="wlModalGrid_CardCtnt_DetTagSectBox">
+                                                    <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagTypeText wlModalGridCard_Type">${show_type}</div>
+                                                </div>
+                                                <div class="wlModalGrid_CardCtnt_DetTagSectBox">
+                                                    <div class="wlModalGrid_CardCtnt_DetTagSectText wlModalGrid_CardCtnt_DetTagReleaseText wlModalGridCard_Year">${show_year}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="wlModalGrid_CardCtnt_DetDescBox">
+                                            <p class="wlModalGrid_CardCtnt_DetDescText">${show_description}</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="wlModalActionFence wlModalAction_GridCardFence">
+                            <div class="wlModalActionBase">
+                                <div class="wlModalActionBdr">
+                                    <div class="wlModalActionBox">
+                                        <button type="button" class="wlModalActionBtn openGenMenuModalBtn" data-gen-menu-modal-type="wl_modal_cards">
+                                            <div class="wlModalActionBtnIcon">
+                                                <svg class="wlModalActionBtnSvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
+                                                    <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/>
+                                                </svg>
+                                            </div>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 `;
                 wlModalGrid_CardBdr.innerHTML = itemStruct;
                 
@@ -1073,7 +1071,6 @@
         wlModalBase.classList.add("active");
         document.body.classList.add("bodystop");
         wlModalGrid_CardBdr = document.querySelectorAll(".wlModalGrid_CardBdr");
-        wlModalGrid_CardBox = document.querySelectorAll(".wlModalGrid_CardBox");
 
         // Closing the modal
         wlModalBaseCloseBtn.addEventListener("click" , () => 
