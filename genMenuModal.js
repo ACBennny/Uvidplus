27fd8f11904fc494aa78fdcad74bca4b1c36c2f9
ACBennny <acbennny1@gmail.com>
2025-01-17 23:58:50 -0400

    WL Modal Header context menu
    I updated the function to call the newly added event listeners.

--- genMenuModal.js
+++ genMenuModal.js
@@ -116,7 +116,7 @@
             menu_ctnt: 
             `
                 <div class="genMenuModalCtntBox wlModalHeaderMenuCtnt">
-                    <button class="genMenuModalCtntBtnBox">
+                    <button class="genMenuModalCtntBtnBox addShowsToWL">
                         <div class="genMenuModalCtntBtnIcon">
                             <svg transform="scale(0.85)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="genMenuModalCtntBtnSvg">
                                 <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
@@ -235,6 +235,7 @@
 
     function wl_modal_header()
     {
+        callGlobalFunctions(`attachAddShowsToWLListeners`);
         callGlobalFunctions(`attachAddToWLEventListeners`);
         callGlobalFunctions(`addDelWLEventListeners`);
     }
