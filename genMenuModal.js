08696936f6c189c84af8191374bc840d9c45d9ed
ACBennny <acbennny1@gmail.com>
2025-01-18 00:31:32 -0400

    Update genMenuModal.js

--- genMenuModal.js
+++ genMenuModal.js
@@ -179,7 +179,7 @@
                         </div>
                         <div class="genMenuModalCtntBtnText">Add to Watchlist</div>
                     </button>
-                    <button class="genMenuModalCtntBtnBox wlModalEditWatchStatusBtn">
+                    <button class="genMenuModalCtntBtnBox wlModalEditWatchStatusBtn openGenMenuModalBtn" data-gen-menu-modal-type="show_watch_status">
                         <div class="genMenuModalCtntBtnIcon">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="genMenuModalCtntBtnSvg">
                                 <path d="m11.4 18.161l7.396-7.396a10.3 10.3 0 0 1-3.326-2.234a10.3 10.3 0 0 1-2.235-3.327L5.839 12.6c-.577.577-.866.866-1.114 1.184a6.6 6.6 0 0 0-.749 1.211c-.173.364-.302.752-.56 1.526l-1.362 4.083a1.06 1.06 0 0 0 1.342 1.342l4.083-1.362c.775-.258 1.162-.387 1.526-.56q.647-.308 1.211-.749c.318-.248.607-.537 1.184-1.114m9.448-9.448a3.932 3.932 0 0 0-5.561-5.561l-.887.887l.038.111a8.75 8.75 0 0 0 2.092 3.32a8.75 8.75 0 0 0 3.431 2.13z" />
@@ -216,6 +216,29 @@
                 </div>
             `,
         },
+        {
+            menu_id: `show_watch_status`,
+            menu_ctnt: 
+            `
+                <div class="genMenuModalCtntBox showWatchStatus">
+                    <button class="genMenuModalCtntBtnBox">
+                        <div class="genMenuModalCtntBtnText">Planned</div>
+                    </button>
+                    <button class="genMenuModalCtntBtnBox">
+                        <div class="genMenuModalCtntBtnText">Watching</div>
+                    </button>
+                    <button class="genMenuModalCtntBtnBox">
+                        <div class="genMenuModalCtntBtnText">On-hold</div>
+                    </button>
+                    <button class="genMenuModalCtntBtnBox">
+                        <div class="genMenuModalCtntBtnText">Completed</div>
+                    </button>
+                    <button class="genMenuModalCtntBtnBox">
+                        <div class="genMenuModalCtntBtnText">Dropped</div>
+                    </button>
+                </div>
+            `,
+        },
     ];
 
     function wl_body_sort()
@@ -243,4 +266,9 @@
     function wl_modal_cards(btnEvent)
     {
         callGlobalFunctions(`attachWLModalCardListeners` , btnEvent);
+    }
+
+    function show_watch_status(btnEvent)
+    {
+        callGlobalFunctions(`attachShowWatchStatusListeners` , btnEvent);
     }
