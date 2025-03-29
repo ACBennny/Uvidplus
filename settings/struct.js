127fa285bd3c27c174db19f5a09daef6abbd437c
ACBennny <acbennny1@gmail.com>
2025-03-29 20:48:48 -0300

    Settings Page
    I have started working on the settings page. This will allow the user manage all aspects of their account and app experience.

---
+++ settings/struct.js
@@ -0,0 +1,33 @@
+/***************************************************************
+ * This will hold the code for the Struct of the Settings page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+    const settingComponents =
+    [
+        {
+            sett_name: ``,
+            sett_struct: 
+            `
+            `,
+        },
+        {
+            sett_name: ``,
+            sett_struct: 
+            `
+            `,
+        },
+        {
+            sett_name: ``,
+            sett_struct: 
+            `
+            `,
+        },
+    ];
+
+
+    
