147e9d1d226bd61bec3fae27018fb12142edc058
ACBennny <acbennny1@gmail.com>
2025-03-29 23:00:31 -0300

    Create app.js
    This will hold the code for the general app processes.

---
+++ app.js
@@ -0,0 +1,7 @@
+/***************************************************************
+ * This will hold the code for the general app processes
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
