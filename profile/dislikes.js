1f569315589e6b80282f0cd71ccc540ed3ac9289
ACBennny <acbennny1@gmail.com>
2025-03-29 21:39:09 -0300

    Likes and Dislikes Functionality
    Rather than the dummy states, users' likes and dislikes are stored for each show, unique to every profile.

---
+++ profile/dislikes.js
@@ -0,0 +1,103 @@
+/***************************************************************
+ * This will serve as a data library for the Watchlist page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+
+    let basicDislikesSliderCardBox;
+
+
+    function preFetchingShowsFromDislikes()
+    {
+        basicDislikesSliderCardBox = document.querySelectorAll(".basic_dislikes_slider_card_box");
+
+        if(basicDislikesSliderCardBox.length <= 0)
+        {
+            notification(`notifyBad` , `Failed to fetch Dislikes`);
+            return;
+        }
+        fetchProfileDislikes();
+    }
+
+
+    function fetchProfileDislikes() 
+    {
+        // Check if content of library is available
+        if(((selectedProfile.prof_dislikes === undefined)))
+        {
+            errorLoadingMyListDislikes();
+            return;
+        }
+        if(((selectedProfile.prof_dislikes.length === 0)))
+        {
+            return;
+        }
+        
+        let struct = ``;
+
+        for(let i = 0; i < selectedProfile.prof_dislikes.length; i++)
+        {
+            const itemId = selectedProfile.prof_dislikes[i].ds_item;
+            let itemIdLC = itemId.split('/')[2];
+            let itemMatch = infoInvLinkMap.get(itemIdLC);
+
+            if(itemMatch)
+            {
+                const {
+                    show_link,
+                    show_title,
+                    show_type,
+                    show_year,
+                    show_thumbnail,
+                } = itemMatch;
+                
+                struct += 
+                `
+                    <div class="slide_card_base max_sizing basic_carousel_card wl_body_card_base">
+                        <div class="slide_card_bdr">
+                            <div class="slide_card_box">
+                                <div class="slide_card">
+                                    <a href="${show_link}" class="cardLinkCover"></a>
+                                    <div class="cardImgBox">
+                                        <img src="${show_thumbnail}" alt="" class="cardImg">
+                                    </div>
+                                    <div class="cardInfoBdr">
+                                        <div class="cardInfoBox">
+                                            <div class="cardInfo_tagBdr">
+                                                <div class="cardInfo_tagBox">
+                                                    <p class="cardInfo_tagText">${show_type}</p>
+                                                </div>
+                                                <div class="cardInfo_tagBox">
+                                                    <p class="cardInfo_tagText">${show_year}</p>
+                                                </div>
+                                            </div>
+                                            <div class="cardInfo_titleBox">
+                                                <h3 class="cardInfo_titleText">${show_title}</h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="slide_card_bcg"></div>
+                        </div>
+                    </div>
+                `;
+            }
+        }
+
+        //
+        basicDislikesSliderCardBox.forEach((box) => 
+        {
+            box.insertAdjacentHTML('beforeend', struct);
+        });
+
+        // Initialize sliding functionalities
+        basicSlider();
+    }
+
+
+
