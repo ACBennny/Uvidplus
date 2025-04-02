eb0cfcf92615f8030a90c47de60c5133e9cb79f0
ACBennny <acbennny1@gmail.com>
2025-04-02 10:34:42 -0300

    Update profile.js
    I removed the return navigation arrow as it has no inherent use.

--- profile/profile.js
+++ profile/profile.js
@@ -151,17 +151,6 @@
         <div class="topNavBdr">
             <div class="topNavBox">
                 <section class="topNav_section">
-                    <div class="sideNavItemsCardBase topNav_openNavNotify">
-                        <button type="button" class="sideNavItemsCardBdr returnToPrevPage" onclick="prev_page_route()">
-                            <div class="sideNavItemsCardBox">
-                                <div class="sideNavItemsCardIcon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="sideNavItemsCardSvg">
-                                        <path fill-rule="evenodd" d="M10.53 5.47a.75.75 0 0 1 0 1.06l-4.72 4.72H20a.75.75 0 0 1 0 1.5H5.81l4.72 4.72a.75.75 0 1 1-1.06 1.06l-6-6a.75.75 0 0 1 0-1.06l6-6a.75.75 0 0 1 1.06 0" clip-rule="evenodd" />
-                                    </svg>
-                                </div>
-                            </div>
-                        </button>
-                    </div>
                 </section>
                 <section class="topNav_section">
                     <div class="sideNavItemsCardBase ">
