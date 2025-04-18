223314efa4a6e35dc81af1e6055f58dd2a0b415b
ACBennny <acbennny1@gmail.com>
2025-04-18 16:31:22 -0300

    Create home-lib.js
    Changed the home library filename from "temp-lib" to "home-lib"

---
+++ home/home-lib.js
@@ -0,0 +1,158 @@
+ 
+/***************************************************************
+ * This will hold some temporary arrays for the Home Page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+// Intro images
+let intro_img_lib = 
+[
+    {
+        intro_link: `#/info/my-hero-academia`,
+    },
+    {
+        intro_link: `#/info/jujutsu-kaisen`,
+    },
+    {
+        intro_link: `#/info/demon-slayer`,
+    },
+    {
+        intro_link: `#/info/one-piece`,
+    },
+    {
+        intro_link: `#/info/apothecary-diaries`,
+    },
+    {
+        intro_link: `#/info/one-punch-man`,
+    },
+    {
+        intro_link: `#/info/dr-stone`,
+    },
+    {
+        intro_link: `#/info/chainsaw-man`,
+    },
+    {
+        intro_link: `#/info/violet-evergarden`,
+    },
+    {
+        intro_link: `#/info/solo-leveling`,
+    },
+    {
+        intro_link: `#/info/kakegurui`,
+    },
+    {
+        intro_link: `#/info/classroom-of-the-elite`,
+    },
+    {
+        intro_link: `#/info/wind-breaker`,
+    },
+    {
+        intro_link: `#/info/spy-x-family`,
+    },
+    {
+        intro_link: `#/info/arcane`,
+    },
+    {
+        intro_link: `#/info/enen-no-shouboutai`,
+    },
+    {
+        intro_link: `#/info/initial-d`,
+    },
+    {
+        intro_link: `#/info/mf-ghost`,
+    },
+    {
+        intro_link: `#/info/black-clover`,
+    },
+    {
+        intro_link: `#/info/hunter-x-hunter`,
+    },
+];
+
+
+// Latest shows
+let latest_shows_lib = 
+[
+    {
+        ls_link: `#/watch/tv/classroom-of-the-elite/3/5`,
+    },
+    {
+        ls_link: `#/watch/tv/bongou-stray-dogs/5/4`,
+    },
+    {
+        ls_link: `#/watch/tv/attack-on-titan/4/4`,
+    },
+    {
+        ls_link: `#/watch/tv/one-piece/1/1125`,
+    },
+    {
+        ls_link: `#/watch/tv/cyberpunk-edgerunners/1/10`,
+    },
+    {
+        ls_link: `#/watch/tv/jujutsu-kaisen/2/23`,
+    },
+    {
+        ls_link: `#/watch/tv/a-sign-of-affection/1/6`,
+    },
+    {
+        ls_link: `#/watch/tv/metallic-rouge/1/2`,
+    },
+    {
+        ls_link: `#/watch/tv/another/1/12`,
+    },
+    {
+        ls_link: `#/watch/tv/solo-levelling/2/11`,
+    },
+    {
+        ls_link: `#/watch/tv/haikyuu/2/1`,
+    },
+    {
+        ls_link: `#/watch/tv/mashle-muscle-and-magic/1/8`,
+    },
+    {
+        ls_link: `#/watch/tv/rent-a-girlfriend/2/10`,
+    },
+    {
+        ls_link: `#/watch/tv/heaven-officials-blessing/1/8`,
+    },
+    {
+        ls_link: `#/watch/tv/demon-slayer/3/7`,
+    },
+    {
+        ls_link: `#/watch/tv/the-wrong-way-to-use-healing-magic/1/9`,
+    },
+];
+
+
+// News
+let news_article_lib = 
+[
+    {
+        na_img: `/images/other-images/chainsaw-man-4.jpg`,
+        na_title: `Chainsaw man..New talks, New Season`,
+        na_date: `19th March 2025`,
+    },
+    {
+        na_img: `/images/other-images/and-you-never-thought-there-was-a-girl-online.jpg`,
+        na_title: `"Anime hits on a differrent level.."`,
+        na_date: `4th February 2025`,
+    },
+    {
+        na_img: `/images/other-images/demon-slayer-roar-of-victory.jpg`,
+        na_title: `Watch Demon Slayer Roar of Victory in theatres`,
+        na_date: `2nd February 2025`,
+    },
+    {
+        na_img: `/images/other-images/goku.jpg`,
+        na_title: `Goku Never dies? Why?`,
+        na_date: `21st January 2025`,
+    },
+    {
+        na_img: `/images/other-images/one-piece-banner-1.jpg`,
+        na_title: `Anime lineups for the year`,
+        na_date: `2nd January 2025`,
+    },
+];
