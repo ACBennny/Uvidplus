a26c3dbfe9783c58a8082519aa47000c97b8e92a
ACBennny <acbennny1@gmail.com>
2025-05-03 13:27:17 -0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
-/**
+/***************************************************************
+ * This will hold the code for the general app processes
  * 
- * 
- * 
- * 
- */
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
 
 
 
