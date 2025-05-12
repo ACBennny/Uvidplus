93fc1abada4bb2026f3b1f2faae20282177d00c5
ACBennny <acbennny1@gmail.com>
2025-05-12 16:08:33 -0300

    Help Center
    I have started working on the help pages.

---
+++ public/help/help.js
@@ -0,0 +1,7 @@
+/***************************************************************
+ * This will hold the default code for the Help pages
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
