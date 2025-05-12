01eb528d0287e29163f9f035f3914dee3064a0ea
ACBennny <acbennny1@gmail.com>
2025-05-11 23:55:28 -0300

    News script
    A minor code to update the navbar active state identifiers when the news page is opened

---
+++ public/news/news-script.js
@@ -0,0 +1,15 @@
+/***************************************************************
+ * This will hold the code for General functions of the news page
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+function _pre_init_news()
+{
+    
+    // Update Navbar active states
+    sideNavLinks[4].classList.add("active");
+}
