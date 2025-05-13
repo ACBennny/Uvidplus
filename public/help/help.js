8af7a47c386a4af2832c60c3165442233398d701
ACBennny <acbennny1@gmail.com>
2025-05-13 03:45:49 -0300

    Help Center Layout & Design
    I am about halfway done.

--- public/help/help.js
+++ public/help/help.js
@@ -5,3 +5,220 @@
  * @version (v0.01)
  *
 ****************************************************************/
+
+
+    const uvid_hlep_struct = 
+    `
+        <div class="help_ctr_bdr">
+            <div class="help_ctr_box">
+                <div class="help_ctr_hdr_bdr">
+                    <div class="help_ctr_hdr_box">
+                        <div class="help_ctr_hdr_bcg_bdr">
+                            <div class="help_ctr_hdr_bcg_box">
+                                <img src="/images/uvid-green-bcg1-dark.jpg" class="help_ctr_hdr_bcg_img">
+                            </div>
+                        </div>
+                        <div class="help_ctr_hdr_ctnt_bdr">
+                            <div class="help_ctr_hdr_ctnt_box">
+                                <div class="help_ctr_hdr_ctnt_greet">
+                                    <div class="help_ctr_hdr_ctnt_ttlBox_mnr">
+                                        <p class="help_ctr_hdr_ctnt_ttlTxt_mnr">Hi! Welcome to the Uvid Help Center</p>
+                                    </div>
+                                    <div class="help_ctr_hdr_ctnt_ttlBox_mjr">
+                                        <span class="help_ctr_hdr_ctnt_ttlTxt_mjr">How can we help?</span>
+                                    </div>
+                                </div>
+                                <div class="help_ctr_hdr_ctnt_srchBdr">
+                                    <div class="help_ctr_hdr_ctnt_srchBox">
+                                        <input type="text" name="help_ctr_hdr_ctnt_srchFld" id="help_ctr_hdr_ctnt_srchFldId" class="help_ctr_hdr_ctnt_srchFldCls" placeholder="Type a question, topic, or issue">
+                                        <div class="help_ctr_hdr_ctnt_srchIcon find_icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="help_ctr_hdr_ctnt_srchSvg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5 2.75a8.75 8.75 0 1 0 0 17.5a8.75 8.75 0 0 0 0-17.5M1.25 11.5c0-5.66 4.59-10.25 10.25-10.25S21.75 5.84 21.75 11.5c0 2.56-.939 4.902-2.491 6.698l3.271 3.272a.75.75 0 1 1-1.06 1.06l-3.272-3.271A10.2 10.2 0 0 1 11.5 21.75c-5.66 0-10.25-4.59-10.25-10.25"/>
+                                            </svg>
+                                        </div>
+                                        <div class="help_ctr_hdr_ctnt_srchIcon clear_icon hide">
+                                            <svg transform="scale(0.85)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="help_ctr_hdr_ctnt_srchSvg">
+                                                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="help_ctr_tpc_bdr">
+                    <div class="help_ctr_tpc_box">
+                        <div class="help_ctr_tpc_sect_bdr">
+                            <div class="help_ctr_tpc_sect_box">
+                                <div class="help_ctr_tpc_sect_hdrBox">
+                                    <div class="help_ctr_tpc_sect_hdrTxt">Popular Topics</div>
+                                </div>
+                                <div class="help_ctr_tpc_sect_gridBdr help_ctr_tpc_pplr_gridBdr">
+                                    <div class="help_ctr_tpc_sect_gridBox help_ctr_tpc_pplr_gridBox">
+
+                                        <a class="help_ctr_tpc_pplr_cardBdr">
+                                            <div class="help_ctr_tpc_pplr_cardBox">
+                                                <div class="help_ctr_tpc_pplr_card_ttlBox">
+                                                    <p class="help_ctr_tpc_pplr_card_ttlTxt">How do I change my email address?</p>
+                                                </div>
+                                                <div class="help_ctr_tpc_pplr_card_iconBdr">
+                                                    <div class="help_ctr_tpc_pplr_card_iconBox">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="help_ctr_tpc_pplr_card_iconSvg">
+                                                            <path fill-rule="evenodd" d="M8.512 4.43a.75.75 0 0 1 1.057.082l6 7a.75.75 0 0 1 0 .976l-6 7a.75.75 0 0 1-1.138-.976L14.012 12L8.431 5.488a.75.75 0 0 1 .08-1.057" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="help_ctr_tpc_sect_bdr">
+                            <div class="help_ctr_tpc_sect_box">
+                                <div class="help_ctr_tpc_sect_hdrBox">
+                                    <div class="help_ctr_tpc_sect_hdrTxt">Explore All Topics</div>
+                                </div>
+                                <div class="help_ctr_tpc_sect_gridBdr help_ctr_tpc_all_gridBdr">
+                                    <div class="help_ctr_tpc_sect_gridBox help_ctr_tpc_all_gridBox">
+
+                                        <a class="help_ctr_tpc_all_cardBdr">
+                                            <div class="help_ctr_tpc_all_cardBox">
+                                                <div class="help_ctr_tpc_all_card_iconBdr">
+                                                    <div class="help_ctr_tpc_all_card_iconBox">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="help_ctr_tpc_all_card_iconSvg">
+                                                            <path d="M20.941 8.189c.58.573.83 1.295.947 2.152c.112.826.112 1.876.112 3.178v.105c0 1.302 0 2.352-.112 3.178c-.117.857-.367 1.579-.947 2.152s-1.31.82-2.178.935c-.835.111-1.898.111-3.216.111h-4.925c-1.318 0-2.38 0-3.216-.111c-.868-.115-1.598-.362-2.178-.935a3 3 0 0 1-.735-1.204c.871.107 1.95.107 3.195.107h5.01c1.281 0 2.385 0 3.27-.117c.947-.126 1.856-.41 2.593-1.138s1.024-1.626 1.151-2.562c.12-.874.12-1.965.119-3.232v-.188c0-1.23 0-2.296-.11-3.158c.46.151.868.379 1.22.727m-10.748.859c-.932 0-1.687.746-1.687 1.666s.755 1.667 1.687 1.667c.931 0 1.686-.746 1.686-1.667c0-.92-.755-1.666-1.686-1.666" />
+                                                            <path fill-rule="evenodd" d="M2.847 5.837C2 6.674 2 8.02 2 10.714s0 4.041.847 4.878s2.21.837 4.936.837h4.82c2.725 0 4.088 0 4.935-.837s.847-2.184.847-4.878s0-4.04-.847-4.877S15.328 5 12.602 5H7.783c-2.726 0-4.09 0-4.936.837m4.213 4.877c0-1.71 1.403-3.095 3.133-3.095s3.132 1.386 3.132 3.095s-1.402 3.096-3.132 3.096s-3.133-1.386-3.133-3.096m8.434 2.62c-.4 0-.723-.32-.723-.715V8.81c0-.394.323-.714.723-.714s.723.32.723.715v3.81a.72.72 0 0 1-.723.713m-11.325-.715c0 .395.323.714.723.714s.722-.32.722-.714V8.81a.72.72 0 0 0-.722-.714c-.4 0-.723.32-.723.715z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </div>
+                                                </div>
+                                                <div class="help_ctr_tpc_all_card_ttlBox">
+                                                    <p class="help_ctr_tpc_all_card_ttlTxt">Account and Billing</p>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!---<div class="help_ctr_more_bdr">
+                    <div class="help_ctr_more_box">
+                        <div class="help_ctr_more_hdr_base">
+                            <div class="help_ctr_more_hdr_bdr">
+                                <div class="help_ctr_more_hdr_box">
+                                    <div class="help_ctr_more_hdr_txt_mjr">Need more help?</div>
+                                </div>
+                                <div class="help_ctr_more_hdr_box">
+                                    <p class="help_ctr_more_hdr_txt_mnr">We are always here & ready to help</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="help_ctr_more_ctnt_bdr">
+                            <div class="help_ctr_more_ctnt_box">
+
+                                <div class="help_ctr_more_ctnt_cardBdr">
+                                    <div class="help_ctr_more_ctnt_cardBox">
+                                        <div class="help_ctr_more_ctnt_card_iconBdr">
+                                            <div class="help_ctr_more_ctnt_card_iconBox">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="help_ctr_more_ctnt_card_iconSvg">
+                                                    <path d="M17 12a5 5 0 1 0-4.478-2.774a.82.82 0 0 1 .067.574l-.298 1.113a.65.65 0 0 0 .796.796l1.113-.298a.82.82 0 0 1 .574.067A5 5 0 0 0 17 12" />
+                                                    <path d="m8.038 7.316l.649 1.163c.585 1.05.35 2.426-.572 3.349c0 0-1.12 1.119.91 3.148c2.027 2.027 3.146.91 3.147.91c.923-.923 2.3-1.158 3.349-.573l1.163.65c1.585.884 1.772 3.106.379 4.5c-.837.836-1.863 1.488-2.996 1.53c-1.908.073-5.149-.41-8.4-3.66c-3.25-3.251-3.733-6.492-3.66-8.4c.043-1.133.694-2.159 1.53-2.996c1.394-1.393 3.616-1.206 4.5.38" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div class="help_ctr_more_ctnt_card_detBdr">
+                                            <div class="help_ctr_more_ctnt_card_detBox">
+                                                <div class="help_ctr_more_ctnt_card_det_ttlBox">
+                                                    <a href="#/contact" class="help_ctr_more_ctnt_card_desc_ttlTxt">Get in Touch</a>
+                                                </div>
+                                                <div class="help_ctr_more_ctnt_card_det_descBox">
+                                                    <p class="help_ctr_more_ctnt_card_det_descTxt">
+                                                        We're happy to assist you, whenever you need us. 
+                                                        Log in to visit our Contact page for further assistance.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="help_ctr_more_ctnt_cardBdr">
+                                    <div class="help_ctr_more_ctnt_cardBox">
+                                        <div class="help_ctr_more_ctnt_card_iconBdr">
+                                            <div class="help_ctr_more_ctnt_card_iconBox">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="help_ctr_more_ctnt_card_iconSvg">
+                                                    <path fill-rule="evenodd" d="M3 10c0-3.771 0-5.657 1.172-6.828S7.229 2 11 2h2c3.771 0 5.657 0 6.828 1.172S21 6.229 21 10v4c0 3.771 0 5.657-1.172 6.828S16.771 22 13 22h-2c-3.771 0-5.657 0-6.828-1.172S3 17.771 3 14zm3 2c0-1.414 0-2.121.44-2.56C6.878 9 7.585 9 9 9h6c1.414 0 2.121 0 2.56.44c.44.439.44 1.146.44 2.56v4c0 1.414 0 2.121-.44 2.56c-.439.44-1.146.44-2.56.44H9c-1.414 0-2.121 0-2.56-.44C6 18.122 6 17.415 6 16zm1-6.75a.75.75 0 0 0 0 1.5h5a.75.75 0 0 0 0-1.5z" clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
+                                        <div class="help_ctr_more_ctnt_card_detBdr">
+                                            <div class="help_ctr_more_ctnt_card_detBox">
+                                                <div class="help_ctr_more_ctnt_card_det_ttlBox">
+                                                    <a href="#/feedback" class="help_ctr_more_ctnt_card_desc_ttlTxt">Send a Feedback</a>
+                                                </div>
+                                                <div class="help_ctr_more_ctnt_card_det_descBox">
+                                                    <p class="help_ctr_more_ctnt_card_det_descTxt">
+                                                        How might we improve Uvid?
+                                                        Use our feedback form to let us know!
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    `;
+    let help_ctr_nav = 
+    `
+        <div class="topNavBdr">
+            <div class="topNavBox">
+                <section class="topNav_section">
+                    <div class="company_logoBdr">
+                        <div class="company_logoBox Companylogo">
+                            <img src="/images/uvid-logo.png" alt="" class="company_logoImg">
+                        </div>
+                    </div>
+                    <a href="#/help" type="button" class="genBtnBox thin transBtn">
+                        <div class="genBtnText">Help Center</div>
+                    </a>
+                </section>
+                <section class="topNav_section"></section>
+            </div>
+        </div>
+    `;
+
+
+    
+    // Initialize
+    function init_help_ctr()
+    {
+        documentCtnt.insertAdjacentHTML('afterbegin' , uvid_hlep_struct);
+
+        // Hide side and bottom navigation
+        sideNavBar.classList.add("hidden");
+        btmNavBar.classList.add("hidden");
+
+        // Insert top navigation
+        topNavBar.classList.add("initialize");
+        topNavBar.innerHTML = help_ctr_nav;
+
+        // Insert Sign In button if user not signed in
+        if(!(getSignedInUser())) document.querySelector(".topNav_section:last-child").insertAdjacentHTML(
+            "beforeend" , 
+            `
+                <a href="#/join" type="button" class="genBtnBox  midSolidBtn">
+                    <div class="genBtnText">Sign In</div>
+                </a>
+            `
+        );
+    }
