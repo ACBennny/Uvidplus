9e046ad7b860a319f8be7a96737903568c37c6de
ACBennny <acbennny1@gmail.com>
2025-05-22 02:39:44 -0300

    Policy Updates
    1. I included some information regarding the future integration of the TMDB API into Uvid.
    
    2. I expanded and provided clarifications on some context of the policies.

--- public/policies/privacy/privacy.js
+++ public/policies/privacy/privacy.js
@@ -22,6 +22,23 @@
                             Uvid does NOT collect or store your personal information, or use
                             any tools or services like <span id="cookies">cookies</span> to do so.
                             <br><br>
+                            To simulate some processes in real streaming services, Uvid may ask you to provide some information, 
+                            which includes but is not limited to your name, phone number, address, postal code, 
+                            card credentials (card name, card number, CVC, etcetera), among others. 
+                            <br><br>
+                            You are expected to <strong>NOT</strong> put any valid information in those fields.
+                            Instead, you may provide any random information as they will not be used for anything other than the process that requires them.
+                            <br><br>
+                            The random information you provide in Uvid will never be used to identify you.
+                            <br><br>
+                            In this respect, you fully, wholefully, and completely <strong>agree to NOT knowingly and/or willfully provide</strong> real, factual, valid or invalid, 
+                            old or new, or any form of information, that directly or indirectly represents you or any other individual
+                            excluding a valid working email for the sole purpose of account related activities which includes but is not limited to 
+                            account creation, account authorisation, account management, and account customization.
+                            <br><br>
+                            Note that this policy is non-applicable to Firebase from Google and the services they provide. 
+                            Click <a class="goToHome" href="https://www.google.com/policies/privacy/">here</a> to view their privacy policy.
+                            <br><br>
                             Note that this policy can be updated at any time without prior notification.
                         </p>
                     </div>
