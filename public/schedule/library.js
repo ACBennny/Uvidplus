58024c89f98d891f45012329f936be5ceb33ac1e
ACBennny <acbennny1@gmail.com>
2025-07-27 02:08:54 -0300

    Delete library.js

--- public/schedule/library.js
+++
@@ -1,7 +0,0 @@
-/***************************************************************
- * This will be the data library for the schedule pages
- * 
- * @author (Anyanwu Benedict Chukwuemeka)
- * @version (v0.01)
- *
-****************************************************************/
