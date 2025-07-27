a870e437c3ca4003cffe87cefb781eadc5883a85
ACBennny <acbennny1@gmail.com>
2025-07-27 20:38:33 -0300

    Regions
    This comprises a list of countries and their two-letter country codes.

---
+++ public/rgn.js
@@ -0,0 +1,449 @@
+/***************************************************************
+ * This will hold regions and their country code
+ * 
+ * @author (Anyanwu Benedict Chukwuemeka)
+ * @version (v0.01)
+ *
+****************************************************************/
+
+
+
+const uvp_regions = 
+[
+    {
+        rg_name: `Afghanistan`,
+        rg_code: `AF`,
+    },
+    {
+        rg_name: `Albania`,
+        rg_code: `AL`,
+    },
+    {
+        rg_name: `American Samoa`,
+        rg_code: `AS`,
+    },
+    {
+        rg_name: `Anguilla`,
+        rg_code: `AI`,
+    },
+    {
+        rg_name: `Argentina`,
+        rg_code: `AR`,
+    },
+    {
+        rg_name: `Armenia`,
+        rg_code: `AM`,
+    },
+    {
+        rg_name: `Australia`,
+        rg_code: `AU`,
+    },
+    {
+        rg_name: `Austria`,
+        rg_code: `AT`,
+    },
+    {
+        rg_name: `Bangladesh`,
+        rg_code: `BD`,
+    },
+    {
+        rg_name: `Barbados`,
+        rg_code: `BB`,
+    },
+    {
+        rg_name: `Belarus`,
+        rg_code: `BY`,
+    },
+    {
+        rg_name: `Belgium`,
+        rg_code: `BE`,
+    },
+    {
+        rg_name: `Benin`,
+        rg_code: `BJ`,
+    },
+    {
+        rg_name: `Bermuda`,
+        rg_code: `BM`,
+    },
+    {
+        rg_name: `Brazil`,
+        rg_code: `BR`,
+    },
+    {
+        rg_name: `Cambodia`,
+        rg_code: `KH`,
+    },
+    {
+        rg_name: `Cameroon`,
+        rg_code: `CM`,
+    },
+    {
+        rg_name: `Canada`,
+        rg_code: `CA`,
+    },
+    {
+        rg_name: `Central African Republic`,
+        rg_code: `CF`,
+    },
+    {
+        rg_name: `Chile`,
+        rg_code: `CL`,
+    },
+    {
+        rg_name: `China`,
+        rg_code: `CN`,
+    },
+    {
+        rg_name: `Cocos (Keeling) Islands`,
+        rg_code: `CC`,
+    },
+    {
+        rg_name: `Colombia`,
+        rg_code: `CO`,
+    },
+    {
+        rg_name: `Costa Rica`,
+        rg_code: `CR`,
+    },
+    {
+        rg_name: `Croatia`,
+        rg_code: `HR`,
+    },
+    {
+        rg_name: `Cuba`,
+        rg_code: `CU`,
+    },
+    {
+        rg_name: `Curaçao`,
+        rg_code: `CW`,
+    },
+    {
+        rg_name: `Cyprus`,
+        rg_code: `CY`,
+    },
+    {
+        rg_name: `Czechia`,
+        rg_code: `CZ`,
+    },
+    {
+        rg_name: `Denmark`,
+        rg_code: `DK`,
+    },
+    {
+        rg_name: `Dominica`,
+        rg_code: `DM`,
+    },
+    {
+        rg_name: `Dominican Republic`,
+        rg_code: `DO`,
+    },
+    {
+        rg_name: `Ecuador`,
+        rg_code: `EC`,
+    },
+    {
+        rg_name: `Egypt`,
+        rg_code: `EG`,
+    },
+    {
+        rg_name: `Equatorial Guinea`,
+        rg_code: `GQ`,
+    },
+    {
+        rg_name: `Estonia`,
+        rg_code: `EE`,
+    },
+    {
+        rg_name: `Finland`,
+        rg_code: `FI`,
+    },
+    {
+        rg_name: `France`,
+        rg_code: `FR`,
+    },
+    {
+        rg_name: `French Guiana`,
+        rg_code: `GF`,
+    },
+    {
+        rg_name: `Gabon`,
+        rg_code: `GA`,
+    },
+    {
+        rg_name: `Gambia`,
+        rg_code: `GM`,
+    },
+    {
+        rg_name: `Georgia`,
+        rg_code: `GE`,
+    },
+    {
+        rg_name: `Germany`,
+        rg_code: `DE`,
+    },
+    {
+        rg_name: `Greece`,
+        rg_code: `GR`,
+    },
+    {
+        rg_name: `Guinea`,
+        rg_code: `GN`,
+    },
+    {
+        rg_name: `Guinea-Bissau`,
+        rg_code: `GW`,
+    },
+    {
+        rg_name: `Hong Kong SAR China`,
+        rg_code: `HK`,
+    },
+    {
+        rg_name: `Hungary`,
+        rg_code: `HU`,
+    },
+    {
+        rg_name: `Iceland`,
+        rg_code: `IS`,
+    },
+    {
+        rg_name: `India`,
+        rg_code: `IN`,
+    },
+    {
+        rg_name: `Indonesia`,
+        rg_code: `ID`,
+    },
+    {
+        rg_name: `Iran`,
+        rg_code: `IR`,
+    },
+    {
+        rg_name: `Ireland`,
+        rg_code: `IR`,
+    },
+    {
+        rg_name: `Israel`,
+        rg_code: `Italy`,
+    },
+    {
+        rg_name: `Japan`,
+        rg_code: `JP`,
+    },
+    {
+        rg_name: `Kazakhstan`,
+        rg_code: `KZ`,
+    },
+    {
+        rg_name: `Kuwait`,
+        rg_code: `KW`,
+    },
+    {
+        rg_name: `Latvia`,
+        rg_code: `LV`,
+    },
+    {
+        rg_name: `Lebanon`,
+        rg_code: `LB`,
+    },
+    {
+        rg_name: `Liberia`,
+        rg_code: `LR`,
+    },
+    {
+        rg_name: `Libya`,
+        rg_code: `LY`,
+    },
+    {
+        rg_name: `Lithuania`,
+        rg_code: `LT`,
+    },
+    {
+        rg_name: `Luxembourg`,
+        rg_code: `LU`,
+    },
+    {
+        rg_name: `Macao SAR China`,
+        rg_code: `MO`,
+    },
+    {
+        rg_name: `Malaysia`,
+        rg_code: `MY`,
+    },
+    {
+        rg_name: `Maldives`,
+        rg_code: `MV`,
+    },
+    {
+        rg_name: `Marshall Islands`,
+        rg_code: `MH`,
+    },
+    {
+        rg_name: `Mexico`,
+        rg_code: `MX`,
+    },
+    {
+        rg_name: `Montenegro`,
+        rg_code: `ME`,
+    },
+    {
+        rg_name: `Myanmar (Burma)`,
+        rg_code: `MM`,
+    },
+    {
+        rg_name: `Nepal`,
+        rg_code: `NP`,
+    },
+    {
+        rg_name: `Netherlands`,
+        rg_code: `NL`,
+    },
+    {
+        rg_name: `New Zealand`,
+        rg_code: `NZ`,
+    },
+    {
+        rg_name: `Nicaragua`,
+        rg_code: `NI`,
+    },
+    {
+        rg_name: `Nigeria`,
+        rg_code: `NG`,
+    },
+    {
+        rg_name: `North Korea`,
+        rg_code: `NK`,
+    },
+    {
+        rg_name: `Norway`,
+        rg_code: `NO`,
+    },
+    {
+        rg_name: `Pakistan`,
+        rg_code: `PK`,
+    },
+    {
+        rg_name: `Paraguay`,
+        rg_code: `PY`,
+    },
+    {
+        rg_name: `Peru`,
+        rg_code: `PE`,
+    },
+    {
+        rg_name: `Philippines`,
+        rg_code: `PH`,
+    },
+    {
+        rg_name: `Poland`,
+        rg_code: `PL`,
+    },
+    {
+        rg_name: `Portugal`,
+        rg_code: `PT`,
+    },
+    {
+        rg_name: `Puerto Rico`,
+        rg_code: `PR`,
+    },
+    {
+        rg_name: `Qatar`,
+        rg_code: `QA`,
+    },
+    {
+        rg_name: `Romania`,
+        rg_code: `RO`,
+    },
+    {
+        rg_name: `Russia`,
+        rg_code: `RU`,
+    },
+    {
+        rg_name: `Saudi Arabia`,
+        rg_code: `SA`,
+    },
+    {
+        rg_name: `Serbia`,
+        rg_code: `RS`,
+    },
+    {
+        rg_name: `Singapore`,
+        rg_code: `SG`,
+    },
+    {
+        rg_name: `Slovakia`,
+        rg_code: `SK`,
+    },
+    {
+        rg_name: `Slovenia`,
+        rg_code: `SI`,
+    },
+    {
+        rg_name: `South Africa`,
+        rg_code: `ZA`,
+    },
+    {
+        rg_name: `South Korea`,
+        rg_code: `KR`,
+    },
+    {
+        rg_name: `Spain`,
+        rg_code: `ES`,
+    },
+    {
+        rg_name: `Sri Lanka`,
+        rg_code: `LK`,
+    },
+    {
+        rg_name: `Sweden`,
+        rg_code: `SE`,
+    },
+    {
+        rg_name: `Switzerland`,
+        rg_code: `CH`,
+    },
+    {
+        rg_name: `Taiwan`,
+        rg_code: `TW`,
+    },
+    {
+        rg_name: `Thailand`,
+        rg_code: `TH`,
+    },
+    {
+        rg_name: `Turkey`,
+        rg_code: `TR`,
+    },
+    {
+        rg_name: `Ukraine`,
+        rg_code: `UA`,
+    },
+    {
+        rg_name: `United Arab Emirates`,
+        rg_code: `AE`,
+    },
+    {
+        rg_name: `United Kingdom`,
+        rg_code: `GB`,
+    },
+    {
+        rg_name: `United States`,
+        rg_code: `US`,
+    },
+    {
+        rg_name: `Uruguay`,
+        rg_code: `UY`,
+    },
+    {
+        rg_name: `Venezuela`,
+        rg_code: `VE`,
+    },
+    {
+        rg_name: `Vietnam`,
+        rg_code: `VN`,
+    },
+];
+
+
+
+
