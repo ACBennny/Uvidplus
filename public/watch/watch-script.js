d6d2826cbd535ed54e003385bfdc4dff4c749fb6
ACBennny <acbennny1@gmail.com>
2025-09-15 00:42:42 -0300

    Update watch-script.js

--- public/watch/watch-script.js
+++ public/watch/watch-script.js
@@ -34,7 +34,7 @@
                     <path fill-rule="evenodd" d="M4.43 8.512a.75.75 0 0 1 1.058-.081L12 14.012l6.512-5.581a.75.75 0 0 1 .976 1.138l-7 6a.75.75 0 0 1-.976 0l-7-6a.75.75 0 0 1-.081-1.057" clip-rule="evenodd" />
                 </svg>
             </div>
-            <span class="genBtnText">Player 1</span>
+            <span class="genBtnText">Player</span>
         </button>
     `;
     let watch_pg_show_link = "";
@@ -414,6 +414,8 @@
             watchPgNextBtn.remove();
             watchPgSelEpBtn.remove();
         }
+
+        document.querySelector(".watch_pg_plyr_sel_btn").querySelector(".genBtnText").textContent = `Player ${extDfltPlyrExt.slice((extDfltPlyrExt.length - 1))}`
 
         // Display base
         watch_pg_tmr = setTimeout(() => 
