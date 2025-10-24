5a8d94418b4bc347cd4c798f21b067f4ee4ad0a6
ACBennny <acbennny1@gmail.com>
2025-10-24 12:09:37 -0300

    Update setup.js
    Additional reminder for users

--- public/join/setup.js
+++ public/join/setup.js
@@ -958,6 +958,7 @@
     // Initialize payment options (Step 3)
     async function init_signup_3()
     {
+        alert("Just a reminder: The payment form you are seeing or about to see is NOT a real payment form and does not require any real information.");
 
         // Check that a plan has been selected
         const usrData = await getUserData();
