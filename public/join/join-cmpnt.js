ad5a25dd8121c333e7fe0b81abf0d2864573740f
ACBennny <acbennny1@gmail.com>
2025-11-14 06:05:03 -0400

    Update join-cmpnt.js

--- public/join/join-cmpnt.js
+++ public/join/join-cmpnt.js
@@ -862,7 +862,7 @@
                                                         <div class="form_pymt_mtd_hdr_ttl_txt">Select payment method</div>
                                                     </div>
                                                     <div class="form_pymt_mtd_hdr_atn_box">
-                                                        <button type="button" class="genBtnBox darkSolidBtn form_pymt_mtd_hdr_atn_btn">
+                                                        <button type="button" class="genBtnBox midSolidBtn form_pymt_mtd_hdr_atn_btn">
                                                             <div class="genBtnIcon">
                                                                 <svg transform="scale(0.85)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="genBtnSvg">
                                                                     <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
