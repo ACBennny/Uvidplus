324646791acbd3dc4bbbe73ef3c6546738ab65a9
c1 <163289555+ACBennny@users.noreply.github.com>
2026-02-12 20:17:27 -0400

    Basic Slider Revamp
    Updated the logic for handling carousels. It should now work regardless of visibility and dimension changes.

--- public/basic-slider.js
+++ public/basic-slider.js
@@ -12,235 +12,245 @@
     let isBasicSliderDown = false;
     let isBasicSliderDragging = false;
     let basicSliderDraggingDist = 10;
-    let singleCardSlide;
-    let multiCardSlide
 
 
 
 // BASIC SLIDERS
 
     // The slider for different shows
-    function basicSlider()
+    // BASIC SLIDERS
+    function basicSlider() 
     {
-        const basic_slider_LeftArrBox = [...document.querySelectorAll('.basic_carousel .arr_left')];
-        const basic_slider_RightArrBox = [...document.querySelectorAll('.basic_carousel .arr_right')];
-        const basic_slider_CardBox = [...document.querySelectorAll('.basic_carousel .basic_carousel_slider')];
-        const basic_slide_CardBase = document.querySelectorAll('.basic_carousel .basic_carousel_card');
-        
-        basic_slider_CardBox.forEach((item, i) => 
+        const carousels = document.querySelectorAll(".basic_carousel");
+
+        carousels.forEach((carousel) => 
         {
-            if(item.mouse_down_atn)
-            {
-                item.removeEventListener("mousedown" , item.mouse_down_atn);
-            }
-            if(item.mouse_leave_atn)
-            {
-                item.removeEventListener("mouseleave" , item.mouse_leave_atn);
-            }
-            if(item.mouse_up_atn)
-            {
-                item.removeEventListener("mouseup" , item.mouse_up_atn);
-            }
-            if(item.mouse_move_atn)
-            {
-                item.removeEventListener("mousemove" , item.mouse_move_atn);
-            }
-            if(item.mouse_scroll_atn)
-            {
-                item.removeEventListener("scroll" , item.mouse_scroll_atn);
-            }
-            if(basic_slider_RightArrBox[i].arrow_right_atn)
-            {
-                basic_slider_RightArrBox[i].removeEventListener("click" , basic_slider_LeftArrBox.arrow_right_atn);
-            }
-            if(basic_slider_LeftArrBox[i].arrow_left_atn)
-            {
-                basic_slider_LeftArrBox[i].removeEventListener("click" , basic_slider_LeftArrBox.arrow_left_atn);
-            }
-        });
+            const slider = carousel.querySelector(".basic_carousel_slider");
+            const leftArr = carousel.querySelector(".arr_left");
+            const rightArr = carousel.querySelector(".arr_right");
+            const cards = carousel.querySelectorAll(".basic_carousel_card");
 
-        basic_slide_CardBase.forEach((base) => 
-        {
-            if(base.card_details_atn)
-            {
-                base.removeEventListener("click" , base.card_details_atn);
-            }
-        });
-        
-        basic_slider_CardBox.forEach((item, i) => 
-        {
-            let basicSliderstartX;
-            let basicSliderScrollLeft;
-            let boxErrorMargin = 10;
-            let boxDimension = item.getBoundingClientRect();
-            let boxWidth = boxDimension.width;
-            let boxW = boxWidth - boxErrorMargin;
-            let boxChildrenDimension = item.querySelectorAll(".basic_carousel_card")[0]?.getBoundingClientRect();
-            let boxChildrenWidth = boxChildrenDimension?.width;
-            multiCardSlide = boxW;
-            singleCardSlide = boxChildrenWidth;
+            if (!slider) return;
 
-            // Unhide/hide right arrow if content is overflowing
-            if((item.scrollWidth) > (Math.ceil((item.clientWidth)) + boxErrorMargin))
+            // Remove old references/listeners
+            const old = slider._basicSliderHandlers;
+
+            if (old) 
             {
-                basic_slider_RightArrBox[i].classList.remove("hide");
+                slider.removeEventListener("mousedown", old.mouse_down_atn);
+                slider.removeEventListener("mouseleave", old.mouse_leave_atn);
+                slider.removeEventListener("mouseup", old.mouse_up_atn);
+                slider.removeEventListener("mousemove", old.mouse_move_atn);
+                slider.removeEventListener("scroll", old.mouse_scroll_atn);
+            }
+            if (rightArr?.arrow_right_atn) 
+            {
+                rightArr.removeEventListener("click", rightArr.arrow_right_atn);
+            }
+            if (leftArr?.arrow_left_atn) 
+            {
+                leftArr.removeEventListener("click", leftArr.arrow_left_atn);
+            }
+            if (slider._basicSliderRO) 
+            {
+                slider._basicSliderRO.disconnect();
             }
 
-            // Unhide/hide left arrow if content is overflowing
-            if((Math.ceil(item.scrollLeft)) > (Math.ceil(multiCardSlide) + boxErrorMargin))
+            cards.forEach((card) => 
             {
-                basic_slider_LeftArrBox[i].classList.remove("hide");
+                if (card.card_details_atn) 
+                {
+                    card.removeEventListener("click", card.card_details_atn);
+                }
+            });
+
+
+            let isDown = false;
+            let isDragging = false;
+            let startX = 0;
+            let startScrollLeft = 0;
+
+            const boxErrorMargin = 10;
+            const getMultiSlide = () => 
+            {
+                const w = slider.getBoundingClientRect().width;
+                return w > 1 ? (w - boxErrorMargin) : 0;
+            };
+
+            const updateArrows = () => 
+            {
+                const slideAmt = getMultiSlide();
+
+                if (!slideAmt) 
+                {
+                    if (leftArr) leftArr.classList.add("hide");
+                    if (rightArr) rightArr.classList.add("hide");
+
+                    return;
+                }
+
+                const scrollLeft = slider.scrollLeft;
+                const canScrollLeft = scrollLeft > boxErrorMargin;
+                const canScrollRight = scrollLeft + slider.clientWidth < slider.scrollWidth - boxErrorMargin;
+
+                if (leftArr) leftArr.classList.toggle("hide", !canScrollLeft);
+                if (rightArr) rightArr.classList.toggle("hide", !canScrollRight);
+            };
+
+            // initial arrow visibility
+            if (rightArr) 
+            {
+                const overflowing = slider.scrollWidth > Math.ceil(slider.clientWidth) + boxErrorMargin;
+                rightArr.classList.toggle("hide", !overflowing);
             }
 
+            updateArrows();
+
+            // -------------------------
+            // HANDLERS
+            // -------------------------
             const mouse_down_atn = (e) => 
             {
-                isBasicSliderDown = true;
-                item.classList.add("isBasicSliderDown");
-                basicSliderstartX = e.pageX - item.offsetLeft;
-                basicSliderScrollLeft = item.scrollLeft;
-                isBasicSliderDragging = false;
-            }
+                isDown = true;
+                slider.classList.add("isBasicSliderDown");
+                startX = e.pageX - slider.offsetLeft;
+                startScrollLeft = slider.scrollLeft;
+                isDragging = false;
+            };
 
             const mouse_leave_atn = () => 
             {
-                isBasicSliderDown = false;
-                item.classList.remove("isBasicSliderDown");
-            }
+                isDown = false;
+                slider.classList.remove("isBasicSliderDown");
+            };
 
             const mouse_up_atn = () => 
             {
-                isBasicSliderDown = false;
-                item.classList.remove("isBasicSliderDown");
-            }
+                isDown = false;
+                slider.classList.remove("isBasicSliderDown");
+            };
 
             const mouse_move_atn = (e) => 
             {
-                // return if mouse is down
-                if (!isBasicSliderDown) return;
-        
+                if (!isDown) return;
+
                 e.preventDefault();
-                const x = e.pageX - item.offsetLeft;
-                const scrollSpeed = (x - basicSliderstartX) * 1;
-                item.scrollLeft = basicSliderScrollLeft - scrollSpeed;
-        
-                // Prevent the cards from being clicked while dragging 
-                if(((Math.abs(x - basicSliderstartX) > basicSliderDraggingDist)))
+                const x = e.pageX - slider.offsetLeft;
+                const walk = (x - startX) * 1;
+                slider.scrollLeft = startScrollLeft - walk;
+
+                if (Math.abs(x - startX) > basicSliderDraggingDist) 
                 {
-                    isBasicSliderDragging = true;
+                    isDragging = true;
                 }
-            }
+            };
 
-            const mouse_scroll_atn = () =>
+            const mouse_scroll_atn = () => updateArrows();
+
+            const arrow_right_atn = () => 
             {
-                let scrollStart = boxErrorMargin;
-                let scrollEnd = item.scrollWidth;
-                let scrollOffWidth = Math.ceil(item.offsetWidth);
-                let currScrollLeft = Math.ceil(item.scrollLeft);
-                let currScroll = Math.ceil((scrollOffWidth + currScrollLeft));
+                const slideAmt = getMultiSlide();
 
-                // Hides Right button
-                if(!(((currScroll) >= scrollEnd) || ((currScroll) >= (scrollEnd - boxErrorMargin))))
-                {
-                    basic_slider_RightArrBox[i].classList.remove("hide");
-                }
-                else
-                {
-                    basic_slider_RightArrBox[i].classList.add("hide");
-                }
+                if (!slideAmt) return;
 
-                // Hides Left button
-                if(!(currScrollLeft <= scrollStart))
-                {
-                    basic_slider_LeftArrBox[i].classList.remove("hide");
-                }
-                else
-                {
-                    basic_slider_LeftArrBox[i].classList.add("hide");
-                }
-            }
+                slider.scrollLeft += slideAmt;
+                updateArrows();
+            };
 
-            const arrow_right_atn = () =>
+            const arrow_left_atn = () => 
             {
-                item.scrollLeft += multiCardSlide;
-            }
+                const slideAmt = getMultiSlide();
 
-            const arrow_left_atn = () =>
-            {
-                item.scrollLeft -= multiCardSlide;
-            }
-    
-            item.addEventListener("mousedown", mouse_down_atn);
-            item.mouse_down_atn = mouse_down_atn;
-        
-            item.addEventListener("mouseleave", mouse_leave_atn);
-            item.mouse_leave_atn = mouse_leave_atn;
-        
-            item.addEventListener("mouseup", mouse_up_atn);
-            item.mouse_up_atn = mouse_up_atn;
-        
-            item.addEventListener("mousemove", mouse_move_atn);
-            item.mouse_move_atn = mouse_move_atn;
+                if (!slideAmt) return;
 
-            item.addEventListener("scroll" , mouse_scroll_atn);
-            item.mouse_scroll_atn = mouse_scroll_atn;
-    
-            // Slides Right
-            basic_slider_RightArrBox[i].addEventListener("click" , arrow_right_atn);
-            basic_slider_RightArrBox[i].arrow_right_atn = arrow_right_atn;
+                slider.scrollLeft -= slideAmt;
+                updateArrows();
+            };
 
-            // Slides Left
-            basic_slider_LeftArrBox[i].addEventListener("click" , arrow_left_atn);
-            basic_slider_LeftArrBox[i].arrow_left_atn = arrow_left_atn;
-        });
-   
-        // Slide Card details (title, alt etc)
-        basic_slide_CardBase.forEach((base) => 
-        {
-            let showCardLink = base.querySelector(".cardLinkCover");
-            let showCards = base.querySelector(".slide_card");
-            let showCardImgs = base.querySelector(".cardImg");
-            let showCardInfoName;
-
-            if((showCards != undefined))
-            {
-                showCardInfoName = showCards.querySelector(".cardInfo_titleText")
-            }
-    
-            // Slide Card Link
-            if((showCardLink != undefined))
-            {
-                showCardLink.draggable = false;
-                showCardLink.style.userSelect = "none";
-            }
-    
-            // Slide card Image
-            if((showCardImgs != undefined) && (showCardInfoName != undefined))
-            {
-                showCardImgs.draggable = false;
-                showCardImgs.style.userSelect = "none";
-                showCardImgs.alt ="Image of the Anime " + showCardInfoName.textContent;
-            }
-    
-            // Slide Card
-            if((showCards != undefined) && (showCardInfoName != undefined))
-            {
-                showCards.title = "Watch " + showCardInfoName.textContent;
-                showCards.draggable = false;
-                showCards.style.userSelect = "none";
-            }
-
-            // Prevents user from mistakenly clicking the card while dragging
-            const card_details_atn = (e) => 
-            {
-                if(isBasicSliderDragging)
-                {
-                    e.preventDefault();
-                    return;
-                }
-            }
             
-            base.addEventListener("click" , card_details_atn);
-            base.card_details_atn = card_details_atn;
+            // Attach listeners & references
+            slider.addEventListener("mousedown", mouse_down_atn);
+            slider.addEventListener("mouseleave", mouse_leave_atn);
+            slider.addEventListener("mouseup", mouse_up_atn);
+            slider.addEventListener("mousemove", mouse_move_atn);
+            slider.addEventListener("scroll", mouse_scroll_atn);
+
+            slider._basicSliderHandlers = 
+            {
+                mouse_down_atn,
+                mouse_leave_atn,
+                mouse_up_atn,
+                mouse_move_atn,
+                mouse_scroll_atn,
+            };
+
+            if (rightArr) 
+            {
+                rightArr.addEventListener("click", arrow_right_atn);
+                rightArr.arrow_right_atn = arrow_right_atn;
+            }
+
+            if (leftArr) 
+            {
+                leftArr.addEventListener("click", arrow_left_atn);
+                leftArr.arrow_left_atn = arrow_left_atn;
+            }
+
+            // -------------------------
+            // CARD DETAILS (same behavior, per carousel dragging flag)
+            // -------------------------
+            cards.forEach((base) => 
+            {
+                let showCardLink = base.querySelector(".cardLinkCover");
+                let showCards = base.querySelector(".slide_card");
+                let showCardImgs = base.querySelector(".cardImg");
+                let showCardInfoName;
+
+                if (showCards) showCardInfoName = showCards.querySelector(".cardInfo_titleText");
+
+                if (showCardLink) 
+                {
+                    showCardLink.draggable = false;
+                    showCardLink.style.userSelect = "none";
+                }
+
+                if (showCardImgs && showCardInfoName) 
+                {
+                    showCardImgs.draggable = false;
+                    showCardImgs.style.userSelect = "none";
+                    showCardImgs.alt = "Image of the Anime " + showCardInfoName.textContent;
+                }
+
+                if (showCards && showCardInfoName) 
+                {
+                    showCards.title = "Watch " + showCardInfoName.textContent;
+                    showCards.draggable = false;
+                    showCards.style.userSelect = "none";
+                }
+
+                const card_details_atn = (e) => 
+                {
+                    if (isDragging) 
+                    {
+                        e.preventDefault();
+                        e.stopPropagation();
+                    }
+                };
+
+                base.addEventListener("click", card_details_atn);
+                base.card_details_atn = card_details_atn;
+            });
+
+            if ("ResizeObserver" in window) 
+            {
+                const ro = new ResizeObserver(() => 
+                {
+                    requestAnimationFrame(updateArrows);
+                });
+
+                ro.observe(slider);
+                slider._basicSliderRO = ro;
+            }
         });
 
         elFc();
@@ -251,11 +261,6 @@
     window.addEventListener("resize" , () => 
     {
         basicSlider();   
-    });
-
-    window.addEventListener("change" , () => 
-    {
-        basicSlider();
     });
 
     
